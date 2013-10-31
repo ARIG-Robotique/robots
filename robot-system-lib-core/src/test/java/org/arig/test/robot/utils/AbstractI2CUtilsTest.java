5d781d9e8e2c79f9b92a6ab32e1aa1254b1833c1
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2013-10-31 15:01:24 +0100

    Site + résolution erreur dummy i2c utils

--- robot-system-lib-core/src/test/java/org/arig/test/robot/utils/AbstractI2CUtilsTest.java
+++ robot-system-lib-core/src/test/java/org/arig/test/robot/utils/AbstractI2CUtilsTest.java
@@ -20,130 +20,135 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 @RunWith(BlockJUnit4ClassRunner.class)
 public class AbstractI2CUtilsTest {
 
-	/** The impl. */
-	private static AbstractI2CUtils impl;
+    /** The impl. */
+    private static AbstractI2CUtils impl;
 
-	/**
-	 * Inits the class.
-	 */
-	@BeforeClass
-	public static void initClass() {
-		AbstractI2CUtilsTest.impl = new AbstractI2CUtils() {
-			@Override
-			protected void scan() throws I2CException {
-				AbstractI2CUtilsTest.log.info("Dummy scan !");
-			}
+    /**
+     * Inits the class.
+     */
+    @BeforeClass
+    public static void initClass() {
+        AbstractI2CUtilsTest.impl = new AbstractI2CUtils() {
+            @Override
+            protected void scan() throws I2CException {
+                AbstractI2CUtilsTest.log.info("Dummy scan !");
+            }
 
-			@Override
-			public void reset() throws I2CException {
-				getBoardMap().clear();
-			}
+            @Override
+            public void reset() throws I2CException {
+                getBoardMap().clear();
+            }
 
-			@Override
-			public boolean isError(final Byte returnCode) {
-				// Unix like implementation
-				return returnCode != 0;
-			}
-		};
-	}
+            @Override
+            public boolean isError(final Byte returnCode) {
+                // Unix like implementation
+                return returnCode != 0;
+            }
 
-	/**
-	 * Inits the test.
-	 */
-	@Before
-	public void initTest() {
-		for (byte b = 1 ; b < 4 ; b++) {
-			AbstractI2CUtilsTest.impl.registerBoard("Board " + b, b);
-		}
-	}
+            @Override
+            public void printError(final Byte returnCode) {
+                AbstractI2CUtilsTest.log.error("Code d'erreur : " + returnCode);
+            }
+        };
+    }
 
-	/**
-	 * Test register board1.
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void testRegisterBoard1() {
-		AbstractI2CUtilsTest.impl.registerBoard(null, null);
-	}
+    /**
+     * Inits the test.
+     */
+    @Before
+    public void initTest() {
+        for (byte b = 1 ; b < 4 ; b++) {
+            AbstractI2CUtilsTest.impl.registerBoard("Board " + b, b);
+        }
+    }
 
-	/**
-	 * Test register board2.
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void testRegisterBoard2() {
-		AbstractI2CUtilsTest.impl.registerBoard(null, (byte) 1);
-	}
+    /**
+     * Test register board1.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testRegisterBoard1() {
+        AbstractI2CUtilsTest.impl.registerBoard(null, null);
+    }
 
-	/**
-	 * Test register board3.
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void testRegisterBoard3() {
-		AbstractI2CUtilsTest.impl.registerBoard("", (byte) -1);
-	}
+    /**
+     * Test register board2.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testRegisterBoard2() {
+        AbstractI2CUtilsTest.impl.registerBoard(null, (byte) 1);
+    }
 
-	/**
-	 * Test register board.
-	 */
-	@Test
-	public void testRegisterBoard() {
-		final int init = AbstractI2CUtilsTest.impl.nbBoardRegistered();
+    /**
+     * Test register board3.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testRegisterBoard3() {
+        AbstractI2CUtilsTest.impl.registerBoard("", (byte) -1);
+    }
 
-		for (byte b = 1 ; b < 4 ; b++) {
-			AbstractI2CUtilsTest.impl.registerBoard("Board Bis " + b, b);
-		}
+    /**
+     * Test register board.
+     */
+    @Test
+    public void testRegisterBoard() {
+        final int init = AbstractI2CUtilsTest.impl.nbBoardRegistered();
 
-		Assert.assertEquals(init + 3, AbstractI2CUtilsTest.impl.nbBoardRegistered());
-	}
+        for (byte b = 1 ; b < 4 ; b++) {
+            AbstractI2CUtilsTest.impl.registerBoard("Board Bis " + b, b);
+        }
 
-	/**
-	 * Test is ok.
-	 */
-	@Test
-	public void testIsOk() {
-		Assert.assertTrue(AbstractI2CUtilsTest.impl.isOk((byte) 0));
-		Assert.assertFalse(AbstractI2CUtilsTest.impl.isOk((byte) 1));
-	}
+        Assert.assertEquals(init + 3, AbstractI2CUtilsTest.impl.nbBoardRegistered());
+    }
 
-	/**
-	 * Test execute scan without mapping.
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void testExecuteScanWithoutMapping() {
-		try {
-			AbstractI2CUtilsTest.impl.reset();
-			AbstractI2CUtilsTest.impl.executeScan();
-		} catch (final I2CException e) {
-			Assert.fail("Pas la bonne exception : " + e.toString());
-		}
-	}
+    /**
+     * Test is ok.
+     */
+    @Test
+    public void testIsOk() {
+        Assert.assertTrue(AbstractI2CUtilsTest.impl.isOk((byte) 0));
+        Assert.assertFalse(AbstractI2CUtilsTest.impl.isOk((byte) 1));
+    }
 
-	/**
-	 * Text execute scan.
-	 *
-	 * @throws I2CException the i2 c exception
-	 */
-	@Test
-	public void textExecuteScan() throws I2CException {
-		AbstractI2CUtilsTest.impl.executeScan();
-	}
+    /**
+     * Test execute scan without mapping.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testExecuteScanWithoutMapping() {
+        try {
+            AbstractI2CUtilsTest.impl.reset();
+            AbstractI2CUtilsTest.impl.executeScan();
+        } catch (final I2CException e) {
+            Assert.fail("Pas la bonne exception : " + e.toString());
+        }
+    }
 
-	/**
-	 * Test get unknown board.
-	 *
-	 * @throws I2CException the i2 c exception
-	 */
-	@Test(expected = I2CException.class)
-	public void testGetUnknownBoard() throws I2CException {
-		AbstractI2CUtilsTest.impl.getBoardAddress("Unknown Board");
-	}
+    /**
+     * Text execute scan.
+     *
+     * @throws I2CException the i2 c exception
+     */
+    @Test
+    public void textExecuteScan() throws I2CException {
+        AbstractI2CUtilsTest.impl.executeScan();
+    }
 
-	/**
-	 * Test get known board.
-	 *
-	 * @throws I2CException the i2 c exception
-	 */
-	@Test
-	public void testGetKnownBoard() throws I2CException {
-		Assert.assertEquals(2, AbstractI2CUtilsTest.impl.getBoardAddress("Board 2"), 0);
-	}
+    /**
+     * Test get unknown board.
+     *
+     * @throws I2CException the i2 c exception
+     */
+    @Test(expected = I2CException.class)
+    public void testGetUnknownBoard() throws I2CException {
+        AbstractI2CUtilsTest.impl.getBoardAddress("Unknown Board");
+    }
+
+    /**
+     * Test get known board.
+     *
+     * @throws I2CException the i2 c exception
+     */
+    @Test
+    public void testGetKnownBoard() throws I2CException {
+        Assert.assertEquals(2, AbstractI2CUtilsTest.impl.getBoardAddress("Board 2"), 0);
+    }
 }
