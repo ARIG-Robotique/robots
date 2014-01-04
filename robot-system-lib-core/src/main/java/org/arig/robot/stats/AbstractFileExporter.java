e532f980442f7b3a8aac056501deaa3ec6fde94b
Gregory DEPUILLE <gregory.depuille@gmail.com>
2014-01-04 11:46:46 +0100

    Implementation CSV

--- robot-system-lib-core/src/main/java/org/arig/robot/stats/AbstractFileExporter.java
+++ robot-system-lib-core/src/main/java/org/arig/robot/stats/AbstractFileExporter.java
@@ -14,6 +14,7 @@ import java.util.List;
 public abstract class AbstractFileExporter implements IStatsExporter {
 
     @Autowired
+    @Getter(AccessLevel.PROTECTED)
     private List<IStatsObject> statsObjectList;
 
     @Getter(AccessLevel.PROTECTED)
