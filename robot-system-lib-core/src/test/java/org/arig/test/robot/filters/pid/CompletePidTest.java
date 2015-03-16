73ac0a55e92b34ad5716d96e3f01683de1e9ef01
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2015-03-16 13:55:20 +0100

    Export CSV pour les data des test des PID et Ramp.

--- robot-system-lib-core/src/test/java/org/arig/test/robot/filters/pid/CompletePidTest.java
+++ robot-system-lib-core/src/test/java/org/arig/test/robot/filters/pid/CompletePidTest.java
@@ -1,5 +1,6 @@
 package org.arig.test.robot.filters.pid;
 
+import com.csvreader.CsvWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.filters.pid.CompletePID;
 import org.arig.robot.filters.pid.IPidFilter;
@@ -12,6 +13,10 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
 /**
  * Created by gdepuille on 15/03/15.
  */
@@ -19,22 +24,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(BlockJUnit4ClassRunner.class)
 public class CompletePidTest {
 
-    private static IPidFilter pid;
+    private static File rootCsvDir;
 
     @BeforeClass
     public static void initClass() {
-        CompletePID pid = new CompletePID();
-        pid.setControllerDirection(IPidFilter.PidType.DIRECT);
-        pid.setMode(IPidFilter.PidMode.AUTOMATIC);
-        pid.setSampleTime(1);
-        pid.reset();
-        pid.initialise();
-
-        CompletePidTest.pid = pid;
+        rootCsvDir = new File(System.getProperty("java.io.tmpdir") + "/arig/robot/pid");
+        if (!rootCsvDir.exists()) {
+            rootCsvDir.mkdirs();
+        }
     }
 
     @Test
-    public void testP() {
+    public void testP() throws Exception {
+        File outputFile = new File(rootCsvDir, "complete-pid-P.csv");
+        CsvWriter writer = new CsvWriter(new OutputStreamWriter(new FileOutputStream(outputFile, false), "UTF-8"), ';');
+        writer.writeRecord(new String[] {"consigne", "input", "output"});
+
+        CompletePID pid = getPid();
         pid.setTunings(1, 0, 0);
 
         double consigne = 100;
@@ -46,6 +52,43 @@ public class CompletePidTest {
             output = pid.compute(consigne, input);
             log.info("Test P : consigne {}, input {}, output {}", consigne, input, output);
             Assert.assertEquals(consigne - input, output, 1);
+            writer.writeRecord(new String[] {String.valueOf(consigne), String.valueOf(input), String.valueOf(output)});
         }
+        writer.flush();
+        writer.close();
+    }
+
+    @Test
+    public void testPI() throws Exception {
+        File outputFile = new File(rootCsvDir, "complete-pid-PI.csv");
+        CsvWriter writer = new CsvWriter(new OutputStreamWriter(new FileOutputStream(outputFile, false), "UTF-8"), ';');
+        writer.writeRecord(new String[] {"consigne", "input", "output"});
+
+        CompletePID pid = getPid();
+        pid.setTunings(1, 0.5, 0);
+
+        double consigne = 100;
+        double input = 0, output = 0;
+        for (int i = 0 ; i < 100 ; i++) {
+            if (i > 10) {
+                input = (i * consigne) / 100;
+            }
+            output = pid.compute(consigne, input);
+            log.info("Test P : consigne {}, input {}, output {}", consigne, input, output);
+            writer.writeRecord(new String[] {String.valueOf(consigne), String.valueOf(input), String.valueOf(output)});
+        }
+        writer.flush();
+        writer.close();
+    }
+
+    private CompletePID getPid() {
+        CompletePID pid = new CompletePID();
+        pid.setControllerDirection(IPidFilter.PidType.DIRECT);
+        pid.setMode(IPidFilter.PidMode.AUTOMATIC);
+        pid.setSampleTime(1);
+        pid.reset();
+        pid.initialise();
+
+        return pid;
     }
 }
--- robot-system-lib-core/src/test/java/org/arig/test/robot/filters/ramp/RampTest.java
+++ robot-system-lib-core/src/test/java/org/arig/test/robot/filters/ramp/RampTest.java
@@ -1,5 +1,6 @@
 package org.arig.test.robot.filters.ramp;
 
+import com.csvreader.CsvWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.filters.pid.IPidFilter;
 import org.arig.robot.filters.pid.SimplePID;
@@ -14,6 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
 /**
  * Created by gdepuille on 15/03/15.
  */
@@ -25,16 +30,33 @@ public class RampTest {
     @Autowired
     private IRampFilter filter;
 
+    private static File rootCsvDir;
+
+    @BeforeClass
+    public static void initClass() {
+        rootCsvDir = new File(System.getProperty("java.io.tmpdir") + "/arig/robot/ramp");
+        if (!rootCsvDir.exists()) {
+            rootCsvDir.mkdirs();
+        }
+    }
+
     @Test
-    public void testFilter() {
+    public void testFilter() throws Exception {
+        File outputFile = new File(rootCsvDir, "ramp.csv");
+        CsvWriter writer = new CsvWriter(new OutputStreamWriter(new FileOutputStream(outputFile, false), "UTF-8"), ';');
+        writer.writeRecord(new String[] {"vitesse", "consigne", "output"});
+
         double vitesse = 100;
         double output;
-        for (int i = 200 ; i > 0 ; i--) {
+        for (int i = 200 ; i >= -10 ; i--) {
             if(i == 100) {
                 vitesse = 150;
             }
             output = filter.filter(vitesse, i, 0, true);
             log.info("Vitesse {}, consigne {}, output {}", vitesse, i, output);
+            writer.writeRecord(new String[]{String.valueOf(vitesse), String.valueOf(i), String.valueOf(output)});
         }
+        writer.flush();
+        writer.close();
     }
 }
