76902da50c8dcfabf82a6250ae4dd9d398883552
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2015-04-26 23:39:10 +0200

    Configuration du WiFi. Même MAC, même IP
    Signed-off-by: Gregory DEPUILLE <gregory.depuille@sogelink.fr>

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo "Compilation ..."
 ./gradlew clean build
 
 echo "Déploiement ..."
-scp ./build/libs/nerell-*-SNAPSHOT.jar 192.168.1.104:/home/pi/nerell/
-scp ./src/main/scripts/* 192.168.1.104:/home/pi/nerell/
+scp ./build/libs/nerell-*-SNAPSHOT.jar 192.168.1.31:/home/pi/nerell/
+scp ./src/main/scripts/* 192.168.1.31:/home/pi/nerell/
