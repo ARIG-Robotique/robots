c3ad39eb7890b4d47948e782c02ab58ba6b44e94
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2017-04-26 00:28:15 +0200

    Plus besoin de configurer la carte d'alimentation avant éxécution car le bug du PCF8574 est corrigé.

--- nerell-parent/nerell-utils/src/main/scripts/checkIOs.sh
+++ nerell-parent/nerell-utils/src/main/scripts/checkIOs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo i2cset -y 1 0x3D 0xF0
+#sudo i2cset -y 1 0x3D 0xF0
 sudo java -cp "nerell-utils.jar:./libs/*" -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y org.arig.robot.CheckIOs
