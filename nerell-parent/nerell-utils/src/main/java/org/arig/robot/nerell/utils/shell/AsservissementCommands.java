76e2207a42324fa33af5651ecfb07dd12cb2894a
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2019-04-30 13:59:49 +0200

    Activation des asserv si l'alimentation est OK

--- nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/AsservissementCommands.java
+++ nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/AsservissementCommands.java
@@ -5,11 +5,14 @@ import org.arig.robot.model.CommandeAsservissementPosition;
 import org.arig.robot.model.CommandeRobot;
 import org.arig.robot.model.RobotStatus;
 import org.arig.robot.model.enums.TypeConsigne;
+import org.arig.robot.services.IIOService;
 import org.arig.robot.utils.ConvertionCarouselUnit;
 import org.arig.robot.utils.ConvertionRobotUnit;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
 
 import javax.validation.constraints.NotNull;
 
@@ -18,13 +21,19 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class AsservissementCommands {
 
+    private final IIOService ioService;
     private final RobotStatus rs;
     private final ConvertionRobotUnit convRobot;
     private final ConvertionCarouselUnit convCarousel;
     private final CommandeRobot cmdRobot;
     private final CommandeAsservissementPosition cmdAsservCarousel;
 
+    public Availability alimentationOk() {
+        return ioService.auOk() && ioService.alimPuissance5VOk() && ioService.alimPuissance12VOk()
+                ? Availability.available() : Availability.unavailable("Les alimentations ne sont pas bonnes");
+    }
 
+    @ShellMethodAvailability("alimentationOk")
     @ShellMethod("Asservissement du robot")
     public void asservRobot(@NotNull TypeConsigne[] typeConsignes, long distance, long orientation) {
         cmdRobot.setTypes(typeConsignes);
@@ -42,6 +51,7 @@ public class AsservissementCommands {
         rs.disableAsserv();
     }
 
+    @ShellMethodAvailability("alimentationOk")
     @ShellMethod("Asservissement du Carousel")
     public void asservCarousel(int index) {
         cmdAsservCarousel.getVitesse().setValue(100);
