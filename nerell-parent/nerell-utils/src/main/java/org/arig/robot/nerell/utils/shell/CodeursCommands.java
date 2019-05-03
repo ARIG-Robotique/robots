ac558f930c7c526fe5aabc23edaf4ca1e2a48cbf
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2019-05-03 17:42:29 +0200

    Capture codeur

--- nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/CodeursCommands.java
+++ nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/CodeursCommands.java
@@ -8,6 +8,7 @@ import org.apache.commons.io.IOUtils;
 import org.arig.robot.services.IIOService;
 import org.arig.robot.system.encoders.Abstract2WheelsEncoders;
 import org.arig.robot.system.encoders.AbstractEncoder;
+import org.arig.robot.system.motors.AbstractMotor;
 import org.arig.robot.system.motors.AbstractPropulsionsMotors;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellCommandGroup;
@@ -30,9 +31,11 @@ public class CodeursCommands {
     private final AbstractEncoder carouselEncoder;
     private final Abstract2WheelsEncoders wheelsEncoders;
     private final AbstractPropulsionsMotors propulsionsMotors;
+    private final AbstractMotor carouselMotor;
     private final IIOService ioService;
 
-    private final List<InfoCapture> infos = new ArrayList<>();
+    private final List<InfoCapturePropulsions> infosPropulsions = new ArrayList<>();
+    private final List<InfoCaptureCarousel> infosCarousel = new ArrayList<>();
 
     public Availability alimentationOk() {
         return ioService.auOk() && ioService.alimPuissance5VOk() && ioService.alimPuissance12VOk()
@@ -56,12 +59,12 @@ public class CodeursCommands {
     @SneakyThrows
     @ShellMethodAvailability("alimentationOk")
     @ShellMethod("Capture des valeurs de codeurs des roues de propulsions")
-    public void captureCodeursRoues() {
+    public void captureCodeursPropulsions() {
         // Vitesse positive
         log.info("Reset codeurs");
         wheelsEncoders.reset();
         for (int vitesse = propulsionsMotors.getStopSpeed(); vitesse <= propulsionsMotors.getMaxSpeed() ; vitesse++) {
-            captureForVitesse(vitesse);
+            capturePropulsionsForVitesse(vitesse);
         }
 
         propulsionsMotors.stopAll();
@@ -71,33 +74,81 @@ public class CodeursCommands {
         log.info("Reset codeurs");
         wheelsEncoders.reset();
         for (int vitesse = propulsionsMotors.getStopSpeed() - 1; vitesse >= propulsionsMotors.getMinSpeed() ; vitesse--) {
-            captureForVitesse(vitesse);
+            capturePropulsionsForVitesse(vitesse);
         }
 
         propulsionsMotors.stopAll();
 
         // Ecriture en CSV
-        List<String> lines = infos.parallelStream()
+        List<String> lines = infosPropulsions.parallelStream()
                 .map(i -> String.format("%s;%s;%s", i.getVitesse(), i.getGauche(), i.getDroit()))
                 .collect(Collectors.toList());
-        IOUtils.writeLines(lines, "\n", new FileOutputStream("capture.csv"), Charset.defaultCharset());
+        IOUtils.writeLines(lines, "\n", new FileOutputStream("capture-propulsions.csv"), Charset.defaultCharset());
     }
 
     @SneakyThrows
-    private void captureForVitesse(int vitesse) {
-        log.info("Vitesse moteur {}", vitesse);
+    @ShellMethodAvailability("alimentationOk")
+    @ShellMethod("Capture des valeurs du codeur du carousel")
+    public void captureCodeurCarousel() {
+        // Vitesse positive
+        log.info("Reset codeurs");
+        carouselEncoder.reset();
+        for (int vitesse = carouselMotor.getStopSpeed(); vitesse <= carouselMotor.getMaxSpeed() ; vitesse++) {
+            captureCarouselForVitesse(vitesse);
+        }
+
+        carouselMotor.stop();
+        Thread.sleep(5000);
+
+        // Vitesse négative
+        log.info("Reset codeurs");
+        carouselEncoder.reset();
+        for (int vitesse = carouselMotor.getStopSpeed() - 1; vitesse >= carouselMotor.getMinSpeed() ; vitesse--) {
+            captureCarouselForVitesse(vitesse);
+        }
+
+        carouselMotor.stop();
+
+        // Ecriture en CSV
+        List<String> lines = infosCarousel.parallelStream()
+                .map(i -> String.format("%s;%s", i.getVitesse(), i.getValue()))
+                .collect(Collectors.toList());
+        IOUtils.writeLines(lines, "\n", new FileOutputStream("capture-carousel.csv"), Charset.defaultCharset());
+    }
+
+    @SneakyThrows
+    private void capturePropulsionsForVitesse(int vitesse) {
+        log.info("Vitesse moteurs propulsions {}", vitesse);
         propulsionsMotors.generateMouvement(vitesse, vitesse);
         for(int mesure = 0 ; mesure < 10 ; mesure++) {
             Thread.sleep(10);
 
             wheelsEncoders.lectureValeurs();
-            infos.add(new InfoCapture(vitesse, wheelsEncoders.getGauche(), wheelsEncoders.getDroit()));
+            infosPropulsions.add(new InfoCapturePropulsions(vitesse, wheelsEncoders.getGauche(), wheelsEncoders.getDroit()));
+        }
+    }
+
+    @SneakyThrows
+    private void captureCarouselForVitesse(int vitesse) {
+        log.info("Vitesse moteur carousel {}", vitesse);
+        carouselMotor.speed(vitesse);
+        for(int mesure = 0 ; mesure < 10 ; mesure++) {
+            Thread.sleep(10);
+
+            carouselEncoder.lectureValeur();
+            infosCarousel.add(new InfoCaptureCarousel(vitesse, carouselEncoder.getValue()));
         }
     }
 
     @Data
-    class InfoCapture {
+    class InfoCapturePropulsions {
         private final int vitesse;
         private final double gauche, droit;
+    }
+
+    @Data
+    class InfoCaptureCarousel {
+        private final int vitesse;
+        private final double value;
     }
 }
