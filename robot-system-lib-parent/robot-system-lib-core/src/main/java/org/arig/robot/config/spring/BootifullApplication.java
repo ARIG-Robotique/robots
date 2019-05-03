7a8a5d729f9bdc48cb8f29789ed16d29ba469997
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2019-05-03 17:47:47 +0200

    Valeur execId par défaut

--- robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/config/spring/BootifullApplication.java
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/config/spring/BootifullApplication.java
@@ -1,6 +1,7 @@
 package org.arig.robot.config.spring;
 
 import javafx.application.Application;
+import org.arig.robot.constants.IConstantesConfig;
 import org.arig.robot.ui.JavaFXApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootifullApplication {
 
     protected static void boot(String ... args) {
+        // Initialisation avec 0 pour disposer d'un fichier tous de même
+        System.setProperty(IConstantesConfig.keyExecutionId, "0");
+
         Application.launch(JavaFXApplication.class, args);
     }
 }
