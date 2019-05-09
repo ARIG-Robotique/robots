5b57067065d732e0095e2ea32c8d3d9f3a8ffbef
Damien SOREL <damien.sorel@sogelink.fr>
2019-05-09 15:13:35 +0200

    Désactive ActionsComands

--- nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/ActionsCommands.java
+++ nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/ActionsCommands.java
@@ -21,81 +21,81 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class ActionsCommands {
 
-    private IIOService ioService;
-    private Ordonanceur ordonanceur;
-
-    private PrendreAtomesDepart prendreAtomesDepart;
-    private PrendrePetitDistributeurEquipe prendrePetitDistributeurEquipe;
-    private PrendreTrouNoirEquipe prendreTrouNoirEquipe;
-    private DeposeAccelerateur deposeAccelerateur;
-    private DeposerBalance deposerBalance;
-    private PrendreGoldenium prendreGoldenium;
-    private PrendreGrandDistributeurEquipe1 prendreGrandDistributeurEquipe1;
-    private PrendreGrandDistributeurEquipe2 prendreGrandDistributeurEquipe2;
-    private PrendreGrandDistributeurEquipe3 prendreGrandDistributeurEquipe3;
-
-    @ShellMethodAvailability
-    public Availability alimentationOk() {
-        return ioService.auOk() && ioService.alimPuissance5VOk() && ioService.alimPuissance12VOk()
-                ? Availability.available() : Availability.unavailable("Les alimentations ne sont pas bonnes");
-    }
-
-    @ShellMethod
-    @SneakyThrows
-    public void callageBordure() {
-        ordonanceur.callageBordure();
-    }
-
-    @ShellMethod
-    public void priseAtomesDepart() {
-        execute(prendreAtomesDepart);
-    }
-
-    @ShellMethod
-    public void priseTrouNoirEquipe() {
-        execute(prendreTrouNoirEquipe);
-    }
-
-    @ShellMethod
-    public void prisePetitDistributeur() {
-        execute(prendrePetitDistributeurEquipe);
-    }
-
-    @ShellMethod
-    public void deposeAccelerateur() {
-        execute(deposeAccelerateur);
-    }
-
-    @ShellMethod
-    public void deposeBalance() {
-        execute(deposerBalance);
-    }
-
-    @ShellMethod
-    public void priseGoldenium() {
-        execute(prendreGoldenium);
-    }
-
-    @ShellMethod
-    public void priseGrandDistributeurEquipe(@NotNull Integer index) {
-        switch (index) {
-            case 1:
-                execute(prendreGrandDistributeurEquipe1);
-                break;
-            case 2:
-                execute(prendreGrandDistributeurEquipe2);
-                break;
-            case 3:
-                execute(prendreGrandDistributeurEquipe3);
-                break;
-            default:
-                log.warn("Index invalide");
-        }
-    }
-
-    private void execute(AbstractAction action) {
-        log.info("Execution de l'action {}, ordre {}, valide {}", action.name(), action.order(), action.isValid());
-        action.execute();
-    }
+//    private IIOService ioService;
+//    private Ordonanceur ordonanceur;
+//
+//    private PrendreAtomesDepart prendreAtomesDepart;
+//    private PrendrePetitDistributeurEquipe prendrePetitDistributeurEquipe;
+//    private PrendreTrouNoirEquipe prendreTrouNoirEquipe;
+//    private DeposeAccelerateur deposeAccelerateur;
+//    private DeposerBalance deposerBalance;
+//    private PrendreGoldenium prendreGoldenium;
+//    private PrendreGrandDistributeurEquipe1 prendreGrandDistributeurEquipe1;
+//    private PrendreGrandDistributeurEquipe2 prendreGrandDistributeurEquipe2;
+//    private PrendreGrandDistributeurEquipe3 prendreGrandDistributeurEquipe3;
+//
+//    @ShellMethodAvailability
+//    public Availability alimentationOk() {
+//        return ioService.auOk() && ioService.alimPuissance5VOk() && ioService.alimPuissance12VOk()
+//                ? Availability.available() : Availability.unavailable("Les alimentations ne sont pas bonnes");
+//    }
+//
+//    @ShellMethod
+//    @SneakyThrows
+//    public void callageBordure() {
+//        ordonanceur.callageBordure();
+//    }
+//
+//    @ShellMethod
+//    public void priseAtomesDepart() {
+//        execute(prendreAtomesDepart);
+//    }
+//
+//    @ShellMethod
+//    public void priseTrouNoirEquipe() {
+//        execute(prendreTrouNoirEquipe);
+//    }
+//
+//    @ShellMethod
+//    public void prisePetitDistributeur() {
+//        execute(prendrePetitDistributeurEquipe);
+//    }
+//
+//    @ShellMethod
+//    public void deposeAccelerateur() {
+//        execute(deposeAccelerateur);
+//    }
+//
+//    @ShellMethod
+//    public void deposeBalance() {
+//        execute(deposerBalance);
+//    }
+//
+//    @ShellMethod
+//    public void priseGoldenium() {
+//        execute(prendreGoldenium);
+//    }
+//
+//    @ShellMethod
+//    public void priseGrandDistributeurEquipe(@NotNull Integer index) {
+//        switch (index) {
+//            case 1:
+//                execute(prendreGrandDistributeurEquipe1);
+//                break;
+//            case 2:
+//                execute(prendreGrandDistributeurEquipe2);
+//                break;
+//            case 3:
+//                execute(prendreGrandDistributeurEquipe3);
+//                break;
+//            default:
+//                log.warn("Index invalide");
+//        }
+//    }
+//
+//    private void execute(AbstractAction action) {
+//        log.info("Execution de l'action {}, ordre {}, valide {}", action.name(), action.order(), action.isValid());
+//        action.execute();
+//    }
 
 }
