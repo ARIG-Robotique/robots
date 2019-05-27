584298a555d87766cbdbea6aa7835b8f3e69f89a
Gregory DEPUILLE <gregory.depuille@gmail.com>
2019-05-28 01:53:07 +0200

    Java 11 + log GC

--- nerell-parent/nerell-robot/src/main/scripts/common.sh
+++ nerell-parent/nerell-robot/src/main/scripts/common.sh
@@ -2,6 +2,7 @@
 
 echo "* Suppression des logs ..."
 rm -vf logs/0-traces.log
+rm -vf gc.log
 echo ""
 
 echo "* Suppression de la socket Lidar"
@@ -9,11 +10,11 @@ sudo rm -vf /tmp/lidar.sock
 echo ""
 
 echo "* Configuration de la JVM"
-#JAVA_HOME=/opt/zulu8.36.0.152-ca-jdk1.8.0_202-linux_aarch32hf
-JAVA_HOME=/opt/bellsoft-jre-9.0.4
+#JAVA_HOME=/opt/bellsoft-jre-9.0.4
+JAVA_HOME=/opt/zulu11.1.8-ca-jdk11-c2-linux_aarch32hf
 PATH=${JAVA_HOME}/bin:${PATH}
 java -version
-JVM_ARGS="-XX:+UseG1GC -XX:MaxGCPauseMillis=5 -Xmx256m -Xms256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m --add-exports java.base/jdk.internal.misc=ALL-UNNAMED --add-exports java.base/jdk.internal.ref=ALL-UNNAMED"
+JVM_ARGS="-XX:+UseG1GC -XX:MaxGCPauseMillis=10 -Xlog:gc:./gc.log -Xmx256m -Xms256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m --add-exports java.base/jdk.internal.misc=ALL-UNNAMED --add-exports java.base/jdk.internal.ref=ALL-UNNAMED"
 DEBUG_ARGS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8787,suspend=y"
 
 echo "  - JVM Args   : ${JVM_ARGS}"
