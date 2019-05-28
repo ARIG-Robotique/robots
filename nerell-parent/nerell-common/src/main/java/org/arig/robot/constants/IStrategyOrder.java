dacd406ed2b168b3f49dc84c9d6041b20c5c607b
Duy Le <ldduy1006@gmail.com>
2019-05-28 20:46:18 +0200

    Stratégie point statique

---
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/constants/IStrategyOrder.java
@@ -0,0 +1,15 @@
+package org.arig.robot.constants;
+
+public interface IStrategyOrder {
+
+    int DEPOSE_ACCELERATEUR = 0;
+    int PRIS_GRAND_DIS_1 = 0;
+    int PRIS_GRAND_DIS_2 = 0;
+    int PRIS_GRAND_DIS_3 = 0;
+
+    int DEPOSE_GOLDENIUM = 0;
+    int PRIS_GOLDENIUM = 0;
+    int DEPOSE_MAGASIN = 0;
+    int DEPOSE_BALANCE = 0;
+    int PRIS_TROU_NOIR = 0;
+}
