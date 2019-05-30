7a0a2a7e23ddc1d0292ac9e30755220b372cca4b
Gregory DEPUILLE <gregory.depuille@gmail.com>
2019-05-31 00:12:59 +0200

    Prendre atome rester devant le tableau redium

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/disabled/atomfactorybasic/Basic1PrendreAtomesDepart.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/disabled/atomfactorybasic/Basic1PrendreAtomesDepart.java
@@ -1,11 +1,11 @@
 package org.arig.robot.strategy.actions.disabled.atomfactorybasic;
 
 import org.arig.robot.constants.IStrategyOrder;
-import org.arig.robot.strategy.actions.disabled.atomfactory.PrendreAtomesDepart;
+import org.arig.robot.strategy.actions.active.PrendreAtomeRedium;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Basic1PrendreAtomesDepart extends PrendreAtomesDepart {
+public class Basic1PrendreAtomesDepart extends PrendreAtomeRedium {
     @Override
     public int order() {
         return IStrategyOrder.PRISE_ATOMES_DEPART;
