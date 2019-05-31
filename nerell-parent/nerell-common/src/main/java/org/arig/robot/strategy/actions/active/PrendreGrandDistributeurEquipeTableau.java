e98daf03828300e8a39b729ddc7b314c014e2d96
Gregory DEPUILLE <gregory.depuille@gmail.com>
2019-05-31 02:42:09 +0200

    Fix erreur de package dans le déplacement

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/PrendreGrandDistributeurEquipeTableau.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/PrendreGrandDistributeurEquipeTableau.java
@@ -1,4 +1,4 @@
-package org.arig.robot.strategy.actions.disabled.atomfactory;
+package org.arig.robot.strategy.actions.active;
 
 import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.model.enums.CouleurPalet;
