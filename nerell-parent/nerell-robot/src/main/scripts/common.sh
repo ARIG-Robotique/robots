dcbb28a29081872a1966ef669fdebe73e96d1085
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-01-31 18:44:34 +0100

    Liste des JVMs sur le Robot

--- nerell-parent/nerell-robot/src/main/scripts/common.sh
+++ nerell-parent/nerell-robot/src/main/scripts/common.sh
@@ -10,8 +10,14 @@ sudo rm -vf /tmp/lidar.sock
 echo ""
 
 echo "* Configuration de la JVM"
+# JVM Bellsoft
 #JAVA_HOME=/opt/bellsoft-jre-9.0.4
-JAVA_HOME=/opt/zulu11.1.8-ca-jdk11-c2-linux_aarch32hf
+#JAVA_HOME=/opt/bellsoft-jre-10.0.2
+JAVA_HOME=/opt/bellsoft-jre-11.0.6
+
+# JVM Azul Zulu
+#JAVA_HOME=/opt/zulu11.1.8-ca-jdk11-c2-linux_aarch32hf
+
 PATH=${JAVA_HOME}/bin:${PATH}
 java -version
 JVM_ARGS="-XX:+UseG1GC -XX:MaxGCPauseMillis=10 -Xlog:gc:./gc.log -Xmx256m -Xms256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m --add-exports java.base/jdk.internal.misc=ALL-UNNAMED --add-exports java.base/jdk.internal.ref=ALL-UNNAMED"
