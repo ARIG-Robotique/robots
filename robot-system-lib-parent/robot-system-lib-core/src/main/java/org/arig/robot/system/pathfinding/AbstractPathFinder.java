12ea74fb9a837e3c52c5f13f7c95e5bdf6925433
Damien SOREL <damien.sorel@sogelink.fr>
2020-02-02 12:09:48 +0100

    Moins d'images de path (fusionne l'avant et l'après)

--- robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/pathfinding/AbstractPathFinder.java
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/pathfinding/AbstractPathFinder.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.model.Chemin;
 import org.arig.robot.model.Point;
 import org.arig.robot.utils.ImageUtils;
-import org.springframework.scheduling.annotation.Async;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -16,11 +15,9 @@ import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-/**
- * @author gdepuille on 30/12/13.
- */
 @Slf4j
 public abstract class AbstractPathFinder implements IPathFinder {
 
@@ -32,7 +29,6 @@ public abstract class AbstractPathFinder implements IPathFinder {
     private File pathDir = new File("./logs/path");
     private final DateTimeFormatter dteFormat = DateTimeFormatter.ISO_DATE_TIME;
 
-    @Async
     public void saveImageForWork(BufferedImage workImage) {
         this.workImage = workImage;
         try {
@@ -45,35 +41,49 @@ public abstract class AbstractPathFinder implements IPathFinder {
         }
     }
 
-    @Async
-    public void saveImageForPath(Point from, Chemin c) {
-        try {
-            BufferedImage img = new BufferedImage(workImage.getWidth(), workImage.getHeight(), workImage.getType());
-            Graphics2D g = img.createGraphics();
-            g.drawImage(workImage, 0, 0, null);
+    protected void saveImageForErrorPath(Point from, Point to) {
+        if (isSaveImages()) {
+            saveImageForPath(Arrays.asList(from, to), true);
+        }
+    }
 
-            g.setBackground(Color.WHITE);
-            Point currentPoint = null;
-            Point precedentPoint = null;
+    protected void saveImageForPath(Point from, Chemin c) {
+        if (isSaveImages()) {
             List<Point> pts = new ArrayList<>();
             pts.add(from);
             pts.addAll(c.getPoints());
-            for (int i = 0; i < pts.size(); i++) {
-                // Couleur du premier et des autres points
-                g.setColor((currentPoint == null) ? Color.GREEN : Color.BLACK);
-                currentPoint = pts.get(i);
+            saveImageForPath(pts, false);
+        }
+    }
 
-                // Couleur du dernier point
-                if (i == pts.size() - 1) {
+    private void saveImageForPath(List<Point> pts, boolean isError) {
+        try {
+            final int l = pts.size();
+
+            BufferedImage img = new BufferedImage(workImage.getWidth(), workImage.getHeight(), workImage.getType());
+            Graphics2D g = img.createGraphics();
+
+            g.drawImage(workImage, 0, 0, null);
+
+            g.setBackground(Color.WHITE);
+
+            Point precedentPoint = null;
+            for (int i = 0; i < l; i++) {
+                Point currentPoint = pts.get(i);
+
+                if (i == l - 1) {
                     g.setColor(Color.RED);
+                } else if (i == 0) {
+                    g.setColor(Color.GREEN);
+                } else {
+                    g.setColor(Color.BLACK);
                 }
-                if (precedentPoint != null) {
+
+                if (precedentPoint != null && !isError) {
                     Color back = g.getColor();
 
                     g.setColor(Color.BLUE);
-                    g.drawLine((int) precedentPoint.getX(), (int) precedentPoint.getY(),
-                            (int) currentPoint.getX(), (int) currentPoint.getY());
-
+                    g.drawLine((int) precedentPoint.getX(), (int) precedentPoint.getY(), (int) currentPoint.getX(), (int) currentPoint.getY());
                     g.setColor(back);
                 }
 
@@ -81,12 +91,21 @@ public abstract class AbstractPathFinder implements IPathFinder {
 
                 precedentPoint = currentPoint;
             }
+
+            if (isError || pts.size() > 2) {
+                g.setColor(isError ? Color.RED : Color.BLUE);
+                g.setStroke(new BasicStroke(isError ? 2 : 1, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10, new float[]{5}, 0));
+                g.drawLine((int) pts.get(0).getX(), (int) pts.get(0).getY(), (int) pts.get(l - 1).getX(), (int) pts.get(l - 1).getX());
+            }
+
             g.dispose();
 
             if (!pathDir.exists()) {
                 log.info("Création du répertoire {} : {}", pathDir.getAbsolutePath(), pathDir.mkdirs());
             }
+
             ImageIO.write(ImageUtils.mirrorX(img), "png", new File(pathDir, dteFormat.format(LocalDateTime.now()) + "-path.png"));
+
         } catch (Exception e) {
             log.error("Impossible d'enregistrer le chemin dans une image : {}", e.toString());
         }
