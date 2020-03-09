b3d7f12a0dec7c3f21d9ed04e379a5cb3ce135c2
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-03-09 22:25:02 +0100

    Modification du lancement Simulateur, et gestion de l'arrêt du context Spring

--- nerell-parent/nerell-robot/src/main/java/org/arig/robot/config/spring/NerellRobot.java
+++ nerell-parent/nerell-robot/src/main/java/org/arig/robot/config/spring/NerellRobot.java
@@ -2,8 +2,11 @@ package org.arig.robot.config.spring;
 
 import org.arig.robot.Ordonanceur;
 import org.arig.robot.constants.IConstantesConfig;
+import org.arig.robot.model.RobotStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -12,13 +15,14 @@ import java.time.format.DateTimeFormatter;
 @SpringBootApplication
 public class NerellRobot {
 
-    public static void main(final String [] args) throws IOException {
+    public static void main(final String[] args) throws IOException {
         // Définition d'un ID unique pour le nommage des fichiers
         final String execId = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         System.setProperty(IConstantesConfig.keyExecutionId, execId);
 
-        SpringApplication.run(NerellRobot.class, args);
-
+        ConfigurableApplicationContext context = SpringApplication.run(NerellRobot.class, args);
         Ordonanceur.getInstance().run();
+        SpringApplication.exit(context);
+        context.close();
     }
 }
--- nerell-parent/nerell-simulator/src/main/java/org/arig/robot/config/spring/NerellSimulator.java
+++ nerell-parent/nerell-simulator/src/main/java/org/arig/robot/config/spring/NerellSimulator.java
@@ -3,8 +3,11 @@ package org.arig.robot.config.spring;
 import lombok.SneakyThrows;
 import org.arig.robot.Ordonanceur;
 import org.arig.robot.constants.IConstantesConfig;
+import org.arig.robot.model.RobotStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,13 +16,15 @@ import java.time.format.DateTimeFormatter;
 public class NerellSimulator {
 
     @SneakyThrows
-    public static void main(final String [] args) {
+    public static void main(final String[] args) {
         // Définition d'un ID unique pour le nommage des fichiers
         final String execId = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         System.setProperty(IConstantesConfig.keyExecutionId, execId);
 
-        SpringApplication.run(NerellSimulator.class, args);
-
+        ConfigurableApplicationContext context = SpringApplication.run(NerellSimulator.class, args);
+        RobotStatus rs = context.getBean(RobotStatus.class);
+        rs.setSimulateur();
         Ordonanceur.getInstance().run();
+        context.close();
     }
 }
