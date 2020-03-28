20fd8b40c357879b6b98522fca9913a6bfb732de
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-03-28 16:21:30 +0100

    Filtre de detection de front (montant, descendant ou les deux)

---
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/filters/common/SignalEdgeFilter.java
@@ -0,0 +1,59 @@
+package org.arig.robot.filters.common;
+
+import lombok.Getter;
+import org.apache.commons.lang3.ArrayUtils;
+import org.arig.robot.filters.IFilter;
+import org.springframework.util.Assert;
+
+public class SignalEdgeFilter implements IFilter<Boolean, Boolean> {
+
+    public static final String INITIAL_VALUE_NULL_MESSAGE = "La valeur initial ne peut être null";
+
+    public enum Type {
+        RISING, FALLING
+    }
+
+    private final Boolean initial;
+    private final Type[] types;
+
+    @Getter
+    private Boolean lastValue;
+
+    public SignalEdgeFilter(Boolean initial, Type ... types) {
+        super();
+
+        Assert.notNull(initial, INITIAL_VALUE_NULL_MESSAGE);
+        this.initial = initial;
+        this.lastValue = initial;
+        this.types = types;
+    }
+
+    @Override
+    public void reset() {
+        lastValue = initial;
+    }
+
+    @Override
+    public Boolean filter(Boolean value) {
+        Assert.notNull(value, FILTER_VALUE_NULL_MESSAGE);
+
+        boolean result;
+        if (types.length == 1) {
+            result = ArrayUtils.contains(types, Type.RISING) ? risingEdge(value) : fallingEdge(value);
+        } else {
+            result = risingEdge(value) || fallingEdge(value);
+        }
+
+        lastValue = value;
+        return result;
+    }
+
+    private boolean risingEdge(boolean value) {
+        return value && !lastValue;
+    }
+
+    private boolean fallingEdge(boolean value) {
+        return !value && lastValue;
+    }
+
+}
---
+++ robot-system-lib-parent/robot-system-lib-core/src/test/java/org/arig/test/robot/filters/common/SignalEdgeFilterTest.java
@@ -0,0 +1,91 @@
+package org.arig.test.robot.filters.common;
+
+import org.arig.robot.filters.IFilter;
+import org.arig.robot.filters.common.SignalEdgeFilter;
+import org.arig.robot.filters.common.SignalEdgeFilter.Type;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class SignalEdgeFilterTest {
+
+    private static SignalEdgeFilter risingFilter;
+    private static SignalEdgeFilter fallingFilter;
+    private static SignalEdgeFilter risingAndFallingFilter;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @BeforeClass
+    public static void initClass() {
+        risingFilter = new SignalEdgeFilter(false, Type.RISING);
+        fallingFilter = new SignalEdgeFilter(false, Type.FALLING);
+        risingAndFallingFilter = new SignalEdgeFilter(false, Type.RISING, Type.FALLING);
+    }
+
+    @Before
+    public void beforeTest() {
+        risingFilter.reset();
+        fallingFilter.reset();
+        risingAndFallingFilter.reset();
+    }
+
+    @Test
+    public void risingValueIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(IFilter.FILTER_VALUE_NULL_MESSAGE);
+
+        risingFilter.filter(null);
+    }
+
+    @Test
+    public void testRisingFilter() {
+        Assert.assertEquals(false, risingFilter.filter(false));
+        Assert.assertEquals(true, risingFilter.filter(true));
+        Assert.assertEquals(false, risingFilter.filter(true));
+        Assert.assertEquals(false, risingFilter.filter(false));
+    }
+
+    @Test
+    public void testFallingFilter() {
+        Assert.assertEquals(false, fallingFilter.filter(false));
+        Assert.assertEquals(false, fallingFilter.filter(true));
+        Assert.assertEquals(false, fallingFilter.filter(true));
+        Assert.assertEquals(true, fallingFilter.filter(false));
+    }
+
+    @Test
+    public void testRisingAndFallingFilter() {
+        Assert.assertEquals(false, risingAndFallingFilter.filter(false));
+        Assert.assertEquals(true, risingAndFallingFilter.filter(true));
+        Assert.assertEquals(false, risingAndFallingFilter.filter(true));
+        Assert.assertEquals(true, risingAndFallingFilter.filter(false));
+    }
+
+    @Test
+    public void testReset() {
+        risingFilter.filter(true);
+        Assert.assertEquals(true, risingFilter.getLastValue());
+        risingFilter.reset();
+        Assert.assertEquals(false, risingFilter.getLastValue());
+    }
+
+    @Test
+    public void testLastValue() {
+        boolean last = risingFilter.getLastValue();
+        boolean value = true;
+        for (double i = 0; i < 10; i++) {
+            risingFilter.filter(value);
+            Assert.assertNotEquals(last, risingFilter.getLastValue());
+            Assert.assertEquals(value, risingFilter.getLastValue());
+            last = value;
+            value = !value;
+        }
+    }
+}
