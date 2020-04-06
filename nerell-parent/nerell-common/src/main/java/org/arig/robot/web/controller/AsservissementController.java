648b309869436fcd272d5fc6d2c25c6d957c8fbc
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-04-06 19:32:27 +0200

    Ajout vitesse dans le controller d'asservissement

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/web/controller/AsservissementController.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/web/controller/AsservissementController.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.constants.IConstantesConfig;
 import org.arig.robot.filters.pid.IPidFilter;
 import org.arig.robot.filters.ramp.IRampFilter;
+import org.arig.robot.model.CommandeRobot;
+import org.arig.robot.system.ITrajectoryManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +21,7 @@ import java.util.Map;
 @Profile(IConstantesConfig.profileMonitoring)
 public class AsservissementController {
 
+    private final CommandeRobot cmdRobot;
     private final IPidFilter pidDistance;
     private final IPidFilter pidOrientation;
     private final IRampFilter rampDistance;
@@ -31,10 +35,12 @@ public class AsservissementController {
         if ("ANGLE".equals(type)) {
             values.putAll(pidOrientation.getTunings());
             values.putAll(rampOrientation.getRamps());
+            values.put("vitesse", (double) cmdRobot.getVitesse().getOrientation());
 
         } else if ("DIST".equals(type)) {
             values.putAll(pidDistance.getTunings());
             values.putAll(rampDistance.getRamps());
+            values.put("vitesse", (double) cmdRobot.getVitesse().getDistance());
 
         } else {
             log.warn("Type d'asservissement invalide");
@@ -49,16 +55,19 @@ public class AsservissementController {
                        @RequestParam("ki") final double ki,
                        @RequestParam("kd") final double kd,
                        @RequestParam("rampAcc") final double rampAcc,
-                       @RequestParam("rampDec") final double rampDec) {
+                       @RequestParam("rampDec") final double rampDec,
+                       @RequestParam("vitesse") final double vitesse) {
         if ("ANGLE".equals(type)) {
             pidOrientation.setTunings(kp, ki, kd);
             pidOrientation.reset();
             rampOrientation.setRamps(rampAcc, rampDec);
+            cmdRobot.getVitesse().setOrientation((long) vitesse);
 
         } else if ("DIST".equals(type)) {
             pidDistance.setTunings(kp, ki, kd);
             pidDistance.reset();
             rampDistance.setRamps(rampAcc, rampDec);
+            cmdRobot.getVitesse().setDistance((long) vitesse);
 
         } else {
             log.warn("Type d'asservissement invalide");
