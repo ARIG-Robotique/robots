1b375e0470d47ad9a90f670e95a656143df75aba
Damien Sorel <damien.sorel@sogelink.fr>
2020-04-08 22:13:29 +0200

    Test du filtrage de points sur Lazy Theta*

--- robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/pathfinding/impl/MultiPathFinderImpl.java
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/pathfinding/impl/MultiPathFinderImpl.java
@@ -245,7 +245,7 @@ public class MultiPathFinderImpl extends AbstractPathFinder {
                 break;
             case LAZY_THETA_STAR:
                 algoFunction = LazyThetaStar::new;
-                algoFiltered = false;
+                algoFiltered = true;
                 break;
             case ANYA16:
                 algoFunction = Anya16::new;
