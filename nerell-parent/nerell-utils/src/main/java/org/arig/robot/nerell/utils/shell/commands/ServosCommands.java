784365ee5a4c5d92805df5d13bea490845aa6f96
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-04-30 19:41:53 +0200

    Configuration des temps d'attente pour les amplitudes max des servos

--- nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/commands/ServosCommands.java
+++ nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/commands/ServosCommands.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.services.IIOService;
 import org.arig.robot.services.ServosService;
+import org.arig.robot.utils.ThreadUtils;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
@@ -19,6 +20,8 @@ public class ServosCommands {
     private final ServosService servosService;
     private final IIOService ioService;
 
+    private final int nbLoop = 3;
+
     public Availability alimentationOk() {
         return ioService.auOk() && ioService.alimPuissance5VOk() && ioService.alimPuissance12VOk()
                 ? Availability.available() : Availability.unavailable("Les alimentations ne sont pas bonnes");
@@ -33,5 +36,114 @@ public class ServosCommands {
     @ShellMethod("Récupèration de tension des servos")
     public void getTension() {
         final double tension = servosService.getTension();
+    }
+
+    @ShellMethod("Configuration attente moustaches")
+    public void configWaitMoustache(int wait) {
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.moustachesOuvert(false);
+            ThreadUtils.sleep(wait);
+            servosService.moustachesFerme(false);
+            ThreadUtils.sleep(wait);
+        }
+    }
+
+    @ShellMethod("Configuration attente bras gauche")
+    public void configWaitBrasGauche(int wait) {
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.brasGauchePhare(false);
+            ThreadUtils.sleep(wait);
+            servosService.brasGaucheFerme(false);
+            ThreadUtils.sleep(wait);
+        }
+    }
+
+    @ShellMethod("Configuration attente bras droit")
+    public void configWaitBrasDroit(int wait) {
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.brasDroitPhare(false);
+            ThreadUtils.sleep(wait);
+            servosService.brasDroitFerme(false);
+            ThreadUtils.sleep(wait);
+        }
+    }
+
+    @ShellMethod("Configuration ascenseur avant")
+    public void configWaitAscenseurAvant(int wait, boolean prise, boolean depose) {
+        if (prise) {
+            servosService.ascenseurAvantBas(true);
+            servosService.pincesAvantOuvert(true);
+            ThreadUtils.sleep(5000);
+            servosService.pincesAvantPrise(true);
+        }
+
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.ascenseurAvantOuvertureMoustache(false);
+            ThreadUtils.sleep(wait);
+            servosService.ascenseurAvantBas(false);
+            ThreadUtils.sleep(wait);
+        }
+
+        if (depose) {
+            servosService.pincesAvantOuvert(false);
+        }
+    }
+
+    @ShellMethod("Configuration ascenseur arriere")
+    public void configWaitAscenseurArriere(int wait, boolean prise, boolean depose) {
+        if (prise) {
+            servosService.pincesArriereOuvert(false);
+            servosService.pivotArriereOuvert(true);
+            servosService.ascenseurArriereEcueil(true);
+            ThreadUtils.sleep(5000);
+            servosService.pincesArriereFerme(true);
+            servosService.ascenseurArriereHaut(true);
+        }
+
+        servosService.pincesArriereFerme(true);
+        servosService.pivotArriereOuvert(true);
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.ascenseurArriereHaut(false);
+            ThreadUtils.sleep(wait);
+            servosService.ascenseurArriereTable(false);
+            ThreadUtils.sleep(wait);
+        }
+
+        if (depose) {
+            servosService.pincesArriereOuvert(true);
+            servosService.ascenseurArriereHaut(true);
+            servosService.pincesArriereFerme(false);
+            servosService.pivotArriereFerme(false);
+        }
+    }
+
+    @ShellMethod("Configuration pivot arriere")
+    public void configWaitPivotArriere(int wait) {
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.pivotArriereOuvert(false);
+            ThreadUtils.sleep(wait);
+            servosService.pivotArriereFerme(false);
+            ThreadUtils.sleep(wait);
+        }
+    }
+
+    @ShellMethod("Configuration pince avant")
+    public void configWaitPinceAvant(int wait) {
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.pincesAvantOuvert(false);
+            ThreadUtils.sleep(wait);
+            servosService.pincesAvantFerme(false);
+            ThreadUtils.sleep(wait);
+        }
+    }
+
+    @ShellMethod("Configuration pince arriere")
+    public void configWaitPinceArriere(int wait) {
+        for (int i = 0 ; i < nbLoop ; i++) {
+            servosService.pincesArriereOuvert(false);
+            ThreadUtils.sleep(wait);
+            servosService.pincesArriereFerme(false);
+            ThreadUtils.sleep(wait);
+        }
     }
 }
