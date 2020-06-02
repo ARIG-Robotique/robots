0b40e5fc380d6936caadc370316f13c3f2080182
Damien SOREL <damien.sorel@sogelink.fr>
2020-06-02 21:52:52 +0200

    Abstract depose grand chenal

---
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/AbstractDeposeGrandPortChenal.java
@@ -0,0 +1,150 @@
+package org.arig.robot.strategy.actions.active;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.arig.robot.constants.IConstantesNerellConfig;
+import org.arig.robot.exception.AvoidingException;
+import org.arig.robot.exception.NoPathFoundException;
+import org.arig.robot.model.*;
+import org.arig.robot.model.enums.SensDeplacement;
+import org.arig.robot.services.IPincesArriereService;
+import org.arig.robot.services.IPincesAvantService;
+import org.arig.robot.strategy.actions.AbstractNerellAction;
+import org.arig.robot.system.ITrajectoryManager;
+import org.arig.robot.utils.TableUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Slf4j
+public abstract class AbstractDeposeGrandPortChenal extends AbstractNerellAction {
+
+    protected enum EPosition {
+        NORD,
+        SUD
+    }
+
+    @Autowired
+    private ITrajectoryManager mv;
+
+    @Autowired
+    protected NerellStatus rs;
+
+    @Autowired
+    private IPincesArriereService pincesArriereService;
+
+    @Autowired
+    private IPincesAvantService pincesAvantService;
+
+    @Autowired
+    private TableUtils tableUtils;
+
+    @Getter
+    private boolean completed = false;
+
+    private SensDeplacement sensEntry = SensDeplacement.AUTO;
+
+    abstract ECouleurBouee getCouleurChenal();
+
+    abstract EPosition getPositionChenal();
+
+    abstract Bouee getBoueeAlternateEntry();
+
+    abstract Point getPointAlternateEntry();
+
+    abstract Chenaux getChenauxFuture();
+
+    @Override
+    protected Point entryPoint() {
+        double x = 460;
+        double y = 1200;
+        if (ETeam.JAUNE == rs.getTeam()) {
+            x = 3000 - x;
+        }
+        final Point central = new Point(x, y);
+
+        if (getBoueeAlternateEntry().prise()) {
+            final Point alternateEntry = getPointAlternateEntry();
+
+            if (tableUtils.distance(alternateEntry) < tableUtils.distance(central)) {
+                sensEntry = SensDeplacement.AVANT;
+                return alternateEntry;
+            }
+        }
+
+        sensEntry = SensDeplacement.ARRIERE;
+        return central;
+    }
+
+    @Override
+    public int order() {
+        Chenaux chenauxFuture = getChenauxFuture();
+
+        if (!rs.pincesAvantEmpty() && rs.phare()) {
+            chenauxFuture.addVert(rs.pincesAvant());
+        }
+
+        int order = chenauxFuture.score() - rs.grandChenaux().score();
+        return order + tableUtils.alterOrder(entryPoint());
+    }
+
+    @Override
+    public boolean isValid() {
+        return isTimeValid() && !rs.inPort() && (!rs.pincesArriereEmpty() || !rs.pincesAvantEmpty());
+    }
+
+    @Override
+    public void execute() {
+        try {
+            mv.setVitesse(IConstantesNerellConfig.vitessePath, IConstantesNerellConfig.vitesseOrientation);
+
+            final Point entry = entryPoint();
+            if (tableUtils.distance(entry) > 100) {
+                rs.enableAvoidance();
+                mv.pathTo(entry);
+            }
+            rs.disableAvoidance();
+
+            double xRef = 225;
+            double yRef = 1200;
+            if (rs.getTeam() == ETeam.JAUNE) {
+                xRef = 3000 - xRef;
+            }
+
+            boolean deposeArriere = false;
+            if (!rs.pincesArriereEmpty()) {
+                deposeArriere = true;
+                mv.gotoPointMM(xRef, getYDepose(yRef, false), false, sensEntry);
+                mv.gotoOrientationDeg(getPositionChenal() == EPosition.NORD ? -90 : 90);
+                pincesArriereService.deposeGrandChenal(getCouleurChenal()); // TODO Dépose partiel
+            }
+
+            if (!rs.pincesAvantEmpty()) {
+                if (deposeArriere) {
+                    mv.avanceMM(35);
+                }
+                mv.gotoPointMM(xRef, getYDepose(yRef, true), true, SensDeplacement.AVANT);
+                pincesAvantService.deposeGrandChenal(getCouleurChenal()); // TODO Dépose partiel
+            }
+
+            mv.gotoPointMM(xRef, yRef, false);
+            pincesAvantService.finaliseDepose();
+            completed = true;
+
+        } catch (NoPathFoundException | AvoidingException e) {
+            updateValidTime();
+            log.error("Erreur d'éxécution de l'action : {}", e.toString());
+        }
+    }
+
+    double getYDepose(double yRef, boolean avant) {
+        int coef = 61 + 32; // Offset pour Y en fonction du type de dépose
+        if (avant) {
+            coef += 30;
+        }
+
+        if (getPositionChenal() == EPosition.NORD) {
+            return yRef + coef;
+        } else {
+            return yRef - coef;
+        }
+    }
+}
--- nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/DeposeGrandPortChenalVert.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/DeposeGrandPortChenalVert.java
@@ -1,43 +1,12 @@
 package org.arig.robot.strategy.actions.active;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.arig.robot.constants.IConstantesNerellConfig;
-import org.arig.robot.exception.AvoidingException;
-import org.arig.robot.exception.NoPathFoundException;
 import org.arig.robot.model.*;
-import org.arig.robot.model.enums.SensDeplacement;
-import org.arig.robot.services.IPincesArriereService;
-import org.arig.robot.services.IPincesAvantService;
-import org.arig.robot.strategy.actions.AbstractNerellAction;
-import org.arig.robot.system.ITrajectoryManager;
-import org.arig.robot.utils.TableUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class DeposeGrandPortChenalVert extends AbstractNerellAction {
-
-    @Autowired
-    private ITrajectoryManager mv;
-
-    @Autowired
-    private NerellStatus rs;
-
-    @Autowired
-    private IPincesArriereService pincesArriereService;
-
-    @Autowired
-    private IPincesAvantService pincesAvantService;
-
-    @Autowired
-    private TableUtils tableUtils;
-
-    @Getter
-    private boolean completed = false;
-
-    private SensDeplacement sensEntry = SensDeplacement.AUTO;
+public class DeposeGrandPortChenalVert extends AbstractDeposeGrandPortChenal {
 
     @Override
     public String name() {
@@ -45,109 +14,39 @@ public class DeposeGrandPortChenalVert extends AbstractNerellAction {
     }
 
     @Override
-    protected Point entryPoint() {
-        double x = 460;
-        double y = 1200;
-        if (ETeam.JAUNE == rs.getTeam()) {
-            x = 3000 - x;
-        }
-        final Point central = new Point(x, y);
-
-        if (rs.getTeam() == ETeam.BLEU && rs.bouee(1).prise()) {
-            // Point entrée ISO phare
-            final Point phare = new Point(Phare.ENTRY_X, Phare.ENTRY_Y);
-
-            double distanceCentral = tableUtils.distance(central);
-            double distancePhare = tableUtils.distance(phare);
-            sensEntry = distanceCentral < distancePhare ? SensDeplacement.ARRIERE : SensDeplacement.AVANT;
-            return distanceCentral < distancePhare ? central : phare;
-
-        } else if (rs.getTeam() == ETeam.JAUNE && rs.bouee(16).prise()) {
-            final Point ecueil = new Point(3000 - EcueilEquipe.ENTRY_X, EcueilEquipe.ENTRY_Y);
-
-            double distanceCentral = tableUtils.distance(central);
-            double distanceEcueil = tableUtils.distance(ecueil);
-            sensEntry = distanceCentral < distanceEcueil ? SensDeplacement.ARRIERE : SensDeplacement.AVANT;
-            return distanceCentral < distanceEcueil ? central : ecueil;
-        }
-
-        sensEntry = SensDeplacement.ARRIERE;
-        return central;
+    ECouleurBouee getCouleurChenal() {
+        return ECouleurBouee.VERT;
     }
 
     @Override
-    public int order() {
-        Chenaux chenauxFuture = rs.grandChenaux().with(null, rs.pincesArriere());
-
-        if (!rs.pincesAvantEmpty() && rs.phare()) {
-            chenauxFuture.addVert(rs.pincesAvant());
-        }
-
-        int order = chenauxFuture.score() - rs.grandChenaux().score();
-        return order + tableUtils.alterOrder(entryPoint());
-    }
-
-    @Override
-    public boolean isValid() {
-        return isTimeValid() && !rs.inPort() && (!rs.pincesArriereEmpty() || !rs.pincesAvantEmpty());
-    }
-
-    @Override
-    public void execute() {
-        try {
-            mv.setVitesse(IConstantesNerellConfig.vitessePath, IConstantesNerellConfig.vitesseOrientation);
-
-            final Point entry = entryPoint();
-            if (tableUtils.distance(entry) > 100) {
-                rs.enableAvoidance();
-                mv.pathTo(entry);
-            }
-            rs.disableAvoidance();
-
-            double xRef = 225;
-            double yRef = 1200;
-            double orientationArriere = -90;
-            if (rs.getTeam() == ETeam.JAUNE) {
-                xRef = 3000 - xRef;
-                orientationArriere = 90;
-            }
-
-            boolean deposeArriere = false;
-            if (!rs.pincesArriereEmpty()) {
-                deposeArriere = true;
-                mv.gotoPointMM(xRef, getYDepose(yRef, false), false, sensEntry);
-                mv.gotoOrientationDeg(orientationArriere);
-                pincesArriereService.deposeGrandChenal(ECouleurBouee.VERT); // TODO Dépose partiel
-            }
-
-            if (!rs.pincesAvantEmpty()) {
-                if (deposeArriere) {
-                    mv.avanceMM(35);
-                }
-                mv.gotoPointMM(xRef, getYDepose(yRef, true), true, SensDeplacement.AVANT);
-                pincesAvantService.deposeGrandChenal(ECouleurBouee.VERT); // TODO Dépose partiel
-            }
-
-            mv.gotoPointMM(xRef, yRef, false);
-            pincesAvantService.finaliseDepose();
-            completed = true;
-
-        } catch (NoPathFoundException | AvoidingException e) {
-            updateValidTime();
-            log.error("Erreur d'éxécution de l'action : {}", e.toString());
-        }
-    }
-
-    private double getYDepose(double yRef, boolean avant) {
-        int coef = 61 + 32; // Offset pour Y en fonction du type de dépose
-        if (avant) {
-            coef += 30;
-        }
-
+    EPosition getPositionChenal() {
         if (rs.getTeam() == ETeam.BLEU) {
-            return yRef + coef;
+            return EPosition.NORD;
         } else {
-            return yRef - coef;
+            return EPosition.SUD;
         }
+    }
+
+    @Override
+    Bouee getBoueeAlternateEntry() {
+        if (rs.getTeam() == ETeam.BLEU) {
+            return rs.bouee(1);
+        } else {
+            return rs.bouee(16);
+        }
+    }
+
+    @Override
+    Point getPointAlternateEntry() {
+        if (rs.getTeam() == ETeam.BLEU) {
+            return new Point(Phare.ENTRY_X, Phare.ENTRY_Y);
+        } else {
+            return new Point(3000 - EcueilEquipe.ENTRY_X, EcueilEquipe.ENTRY_Y);
+        }
+    }
+
+    @Override
+    Chenaux getChenauxFuture() {
+        return rs.grandChenaux().with(null, rs.pincesArriere());
     }
 }
