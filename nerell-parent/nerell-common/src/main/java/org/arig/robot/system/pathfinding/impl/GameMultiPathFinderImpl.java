cd19280d533ce582463da55dfb55200095ae9354
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-06-27 13:23:00 +0200

    Exclusion des bouées devant les petit port des obstacles dynamique

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/system/pathfinding/impl/GameMultiPathFinderImpl.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/system/pathfinding/impl/GameMultiPathFinderImpl.java
@@ -3,6 +3,7 @@ package org.arig.robot.system.pathfinding.impl;
 import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.constants.IConstantesNerellConfig;
 import org.arig.robot.model.Bouee;
+import org.arig.robot.model.ETeam;
 import org.arig.robot.model.NerellRobotStatus;
 import org.arig.robot.utils.TableUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,10 @@ public class GameMultiPathFinderImpl extends MultiPathFinderImpl {
         // Ajout des obstacles en fonctions des bouées
         int[] boueesAvoided = new int[]{5, 6, 7, 8, 9, 10, 11, 12};
         for (int nb : boueesAvoided) {
+            if ((rs.getTeam() == ETeam.BLEU && nb == 9) || (rs.getTeam() == ETeam.JAUNE && nb == 8)) {
+                continue;
+            }
+
             Bouee bouee = rs.bouee(nb);
             if (!bouee.prise()) {
                 obstacles.add(tableUtils.createPolygonObstacle(bouee.pt(), IConstantesNerellConfig.pathFindingTailleBouee));
