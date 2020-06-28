1267110340fe59825bf142b31eb5cdb333d224d5
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-06-28 15:24:10 +0200

    Exclusion bouée devant écueil adverse dans les obstacles

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/system/pathfinding/impl/GameMultiPathFinderImpl.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/system/pathfinding/impl/GameMultiPathFinderImpl.java
@@ -3,6 +3,7 @@ package org.arig.robot.system.pathfinding.impl;
 import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.constants.IConstantesNerellConfig;
 import org.arig.robot.model.Bouee;
+import org.arig.robot.model.EStrategy;
 import org.arig.robot.model.ETeam;
 import org.arig.robot.model.NerellRobotStatus;
 import org.arig.robot.utils.TableUtils;
@@ -25,7 +26,12 @@ public class GameMultiPathFinderImpl extends MultiPathFinderImpl {
         // Ajout des obstacles en fonctions des bouées
         int[] boueesAvoided = new int[]{5, 6, 7, 8, 9, 10, 11, 12};
         for (int nb : boueesAvoided) {
+            // ignore la bouée devant le petit port
             if ((rs.getTeam() == ETeam.BLEU && nb == 9) || (rs.getTeam() == ETeam.JAUNE && nb == 8)) {
+                continue;
+            }
+            // ignore la bouée devant l'ecueil adverse en aggressif
+            if (rs.getStrategy() == EStrategy.AGGRESSIVE && ((rs.getTeam() == ETeam.BLEU && nb == 11) || (rs.getTeam() == ETeam.JAUNE && nb == 6))) {
                 continue;
             }
 
