a6c390b4930c3458e32d186e1d82b625cb57c894
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2021-06-07 23:27:05 +0200

    Haut fond dans le sens le plus proche quelque soit l'équipe

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/HautFond.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/HautFond.java
@@ -7,6 +7,7 @@ import org.arig.robot.exception.NoPathFoundException;
 import org.arig.robot.model.Bouee;
 import org.arig.robot.model.ETeam;
 import org.arig.robot.model.Point;
+import org.arig.robot.model.enums.GotoOption;
 import org.arig.robot.services.INerellIOService;
 import org.arig.robot.strategy.actions.AbstractNerellAction;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,15 +37,25 @@ public class HautFond extends AbstractNerellAction {
 
     @Override
     public Point entryPoint() {
-        if (rs.team() == ETeam.BLEU) {
+        if (position.getPt().getX() <= 1500) {
             return new Point(X, Y);
         } else {
             return new Point(3000 - X, Y);
         }
     }
 
+    private Point finalPoint(double finalY) {
+        if (position.getPt().getX() <= 1500) {
+            return new Point(3000 - 1130, finalY);
+        } else {
+            return new Point(1130, finalY);
+        }
+    }
+
+
     @Override
     public int order() {
+        // TODO : Calcule de point avec les paires
         int libre = 0;
         if (!io.presenceVentouse1()) libre++;
         if (!io.presenceVentouse2()) libre++;
@@ -65,20 +76,18 @@ public class HautFond extends AbstractNerellAction {
             final int nbHautFond = rs.hautFond().size();
             final double medianY = rs.hautFond().stream().map(Bouee::pt).mapToDouble(Point::getY).sorted()
                     .skip((nbHautFond - 1) / 2).limit(2 - nbHautFond % 2).average().orElse(entry.getY());
+            final double finalY = Math.min(entry.getY(), medianY);
 
+            entry.setY(finalY);
+            final Point finalPoint = finalPoint(finalY);
+
+            // On y va
             mv.setVitesse(robotConfig.vitesse(), robotConfig.vitesseOrientation());
-            mv.pathTo(entry.getX(), Math.min(entry.getY(), medianY));
-
-            mv.setVitesse(robotConfig.vitesse(30), robotConfig.vitesseOrientation());
-
-            if (rs.team() == ETeam.BLEU) {
-                mv.gotoOrientationDeg(0);
-            } else {
-                mv.gotoOrientationDeg(180);
-            }
+            mv.pathTo(entry);
 
             // on ratisse en laissant l'évitement actif
-            mv.avanceMM(1000);
+            mv.setVitesse(robotConfig.vitesse(30), robotConfig.vitesseOrientation());
+            mv.gotoPoint(finalPoint, GotoOption.AVANT);
 
             // on marque tout comme pris, l'information mise à jour sera fournie par la balise
             rs.hautFond(Collections.emptyList());
