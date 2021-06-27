2328e3310a66d9470d8594218f539465a2197f7a
Damien Sorel <damien.sorel@sogelink.fr>
2021-06-27 19:52:43 +0200

    Fix #129 Optim nettoyage grand port Odin

--- odin-parent/odin-common/src/main/java/org/arig/robot/services/IOdinPincesService.java
+++ odin-parent/odin-common/src/main/java/org/arig/robot/services/IOdinPincesService.java
@@ -3,6 +3,10 @@ package org.arig.robot.services;
 public interface IOdinPincesService {
     void deposeGrandPort();
 
+    void deposeGrandChenalRouge();
+
+    void deposeGrandChenalVert();
+
     void activate();
 
     boolean processBouee();
