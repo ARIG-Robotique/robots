551678c5ba39d1ec96809553123ca9622e385492
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2021-07-09 01:39:41 +0200

    Exclusions des bouées 7 et 10 si action écueil commun en cours

--- odin-parent/odin-common/src/main/java/org/arig/robot/strategy/actions/active/OdinBouee10.java
+++ odin-parent/odin-common/src/main/java/org/arig/robot/strategy/actions/active/OdinBouee10.java
@@ -1,6 +1,12 @@
 package org.arig.robot.strategy.actions.active;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.arig.robot.constants.IEurobotConfig;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class OdinBouee10 extends AbstractOdinBouee {
@@ -8,5 +14,10 @@ public class OdinBouee10 extends AbstractOdinBouee {
     public OdinBouee10() {
         super(10);
     }
-
+    @Getter
+    @Accessors(fluent = true)
+    public List<String> blockingActions = Arrays.asList(
+            IEurobotConfig.ACTION_ECUEIL_COMMUN_JAUNE,
+            IEurobotConfig.ACTION_PRISE_BOUEE_AGGRESSIVE
+    );
 }
--- odin-parent/odin-common/src/main/java/org/arig/robot/strategy/actions/active/OdinBouee7.java
+++ odin-parent/odin-common/src/main/java/org/arig/robot/strategy/actions/active/OdinBouee7.java
@@ -1,6 +1,12 @@
 package org.arig.robot.strategy.actions.active;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.arig.robot.constants.IEurobotConfig;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class OdinBouee7 extends AbstractOdinBouee {
@@ -9,4 +15,10 @@ public class OdinBouee7 extends AbstractOdinBouee {
         super(7);
     }
 
+    @Getter
+    @Accessors(fluent = true)
+    public List<String> blockingActions = Arrays.asList(
+            IEurobotConfig.ACTION_ECUEIL_COMMUN_BLEU,
+            IEurobotConfig.ACTION_PRISE_BOUEE_AGGRESSIVE
+    );
 }
