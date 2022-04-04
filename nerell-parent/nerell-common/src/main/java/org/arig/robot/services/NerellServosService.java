032e65746b9153c1b9df37fcbefe6b2192793a86
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2022-04-04 11:22:17 +0200

    Config Servos Nerell

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/services/NerellServosService.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/services/NerellServosService.java
@@ -30,65 +30,76 @@ public class NerellServosService extends AbstractCommonServosService {
 
         Servo brasBasEpaule = servo(BRAS_BAS_EPAULE_ID, BRAS_BAS_EPAULE)
                 .time(500)
-                .position(POS_REPOS, 1500);
+                .min(960).angleMax(90)
+                .max(1880).angleMin(0)
+                .position(POS_REPOS, 860);
         Servo brasBasCoude = servo(BRAS_BAS_COUDE_ID, BRAS_BAS_COUDE)
                 .time(500)
-                .position(POS_REPOS, 1500);
+                .min(640).angleMax(90)
+                .max(2410).angleMin(-90)
+                .position(POS_REPOS, 1070);
         Servo brasBasPoignet = servo(BRAS_BAS_POIGNET_ID, BRAS_BAS_POIGNET)
                 .time(500)
-                .position(POS_REPOS, 1500);
+                .min(549).angleMax(90)
+                .max(2360).angleMin(-90)
+                .position(POS_REPOS, 2040);
         group(GROUP_BRAS_BAS_ID, GROUP_BRAS_BAS)
                 .addServo(brasBasEpaule)
                 .addServo(brasBasCoude)
-                .addServo(brasBasPoignet)
-                .batch(POS_FERME);
+                .addServo(brasBasPoignet);
 
         Servo brasHautEpaule = servo(BRAS_HAUT_EPAULE_ID, BRAS_HAUT_EPAULE)
                 .time(500)
-                .position(POS_REPOS, 1500);
+                .min(1360).angleMin(-90)
+                .max(2350).angleMax(0)
+                .position(POS_REPOS, 1350);
         Servo brasHautCoude = servo(BRAS_HAUT_COUDE_ID, BRAS_HAUT_COUDE)
                 .time(500)
-                .position(POS_REPOS, 1500);
+                .min(690).angleMin(-90)
+                .max(1620).angleMax(0)
+                .position(POS_REPOS, 1760);
         Servo brasHautPoignet = servo(BRAS_HAUT_POIGNET_ID, BRAS_HAUT_POIGNET)
                 .time(500)
-                .position(POS_REPOS, 1500);
+                .min(560).angleMin(-90)
+                .max(2370).angleMax(90)
+                .position(POS_REPOS, 1680);
         group(GROUP_BRAS_HAUT_ID, GROUP_BRAS_HAUT)
                 .addServo(brasHautEpaule)
                 .addServo(brasHautCoude)
-                .addServo(brasHautPoignet)
-                .batch(POS_FERME);
+                .addServo(brasHautPoignet);
 
         Servo carreFouilleOhmmetre = servo(CARRE_FOUILLE_OHMMETRE_ID, CARRE_FOUILLE_OHMMETRE)
                 .time(500)
-                .position(POS_FERME, 1500)
-                .position(POS_MESURE, 1500);
+                .position(POS_FERME, 2210)
+                .position(POS_OUVERT, 1770)
+                .position(POS_MESURE, 1570);
         Servo carreFouillePoussoir = servo(CARRE_FOUILLE_POUSSOIR_ID, CARRE_FOUILLE_POUSSOIR)
                 .time(500)
-                .position(POS_FERME, 1500)
-                .position(POS_POUSSETTE, 1500);
+                .position(POS_FERME, 450)
+                .position(POS_POUSSETTE, 1050);
         group(GROUP_BRAS_MESURE_ID, GROUP_BRAS_MESURE)
                 .addServo(carreFouilleOhmmetre)
                 .addServo(carreFouillePoussoir);
 
         Servo fourcheStatuette = servo(FOURCHE_STATUETTE_ID, FOURCHE_STATUETTE)
                 .time(500)
-                .position(POS_FERME, 1500)
-                .position(POS_PRISE_DEPOSE, 1500);
+                .position(POS_FERME, 2270)
+                .position(POS_PRISE_DEPOSE, 960);
         group(GROUP_STATUETTE_ID, GROUP_STATUETTE)
                 .addServo(fourcheStatuette);
 
         Servo moustacheGauche = servo(MOUSTACHE_GAUCHE_ID, MOUSTACHE_GAUCHE)
                 .time(500)
-                .position(POS_FERME, 1500)
-                .position(POS_OUVERT, 1500);
+                .position(POS_FERME, 2280)
+                .position(POS_OUVERT, 1370);
         Servo langue = servo(LANGUE_ID, LANGUE)
                 .time(500)
-                .position(POS_FERME, 1500)
-                .position(POS_OUVERT, 1500);
+                .position(POS_FERME, 2270)
+                .position(POS_OUVERT, 1330);
         Servo moustacheDroite = servo(MOUSTACHE_DROITE_ID, MOUSTACHE_DROITE)
                 .time(500)
-                .position(POS_FERME, 1500)
-                .position(POS_OUVERT, 1500);
+                .position(POS_FERME, 670)
+                .position(POS_OUVERT, 1520);
 
         group(GROUP_ARRIERE_ID, GROUP_ARRIERE)
                 .addServo(moustacheGauche)
