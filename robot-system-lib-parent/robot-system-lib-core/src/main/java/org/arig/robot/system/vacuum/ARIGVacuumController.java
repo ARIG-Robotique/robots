502c764c1283aa237749863c6eb744a88adf64af
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2022-04-07 01:19:19 +0200

    Optimisation des communication multi thread sur la carte pompe

--- robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/vacuum/ARIGVacuumController.java
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/vacuum/ARIGVacuumController.java
@@ -7,6 +7,9 @@ import org.arig.robot.utils.ThreadUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class ARIGVacuumController extends AbstractARIGVacuumController {
@@ -16,6 +19,8 @@ public class ARIGVacuumController extends AbstractARIGVacuumController {
     private static final byte WRITE_PUMPS_MODE = 0x30;
 
     private final String deviceName;
+
+    private final ReentrantLock accessLock = new ReentrantLock(true);
 
     @Autowired
     private I2CManager i2cManager;
@@ -27,7 +32,7 @@ public class ARIGVacuumController extends AbstractARIGVacuumController {
     public void readAllValues() {
         log.debug("Lecture de toutes les pompes");
         byte[] data = readFromController(READ_ALL_PUMPS, 8);
-        for (int idx = 0 , bits = 0 ; idx < NB_PUMPS ; idx++, bits += 2) {
+        for (int idx = 0, bits = 0; idx < NB_PUMPS; idx++, bits += 2) {
             pumpData[idx].vacuum(((data[bits] & 0x0F) << 8) + (data[bits + 1] & 0xFF));
             pumpData[idx].presence((data[bits] >> 7 & 0x01) == 1);
             pumpData[idx].tor((data[bits] >> 6 & 0x01) == 1);
@@ -45,46 +50,72 @@ public class ARIGVacuumController extends AbstractARIGVacuumController {
     }
 
     public void printVersion() {
-        try {
-            i2cManager.sendData(deviceName, VERSION_REGISTER, VERSION_REGISTER);
-            final byte[] data = i2cManager.getData(deviceName, 19);
-            final String version = new String(data, StandardCharsets.UTF_8);
-            log.info("ARIG Vacuum Controller version {}, {} pompes", version, NB_PUMPS);
-        } catch (I2CException e) {
-            log.error("Erreur lors de la récupération de la version de la carte ARIG Vacuum controller");
-        }
+        execWithLock("printVersion", () -> {
+            try {
+                i2cManager.sendData(deviceName, VERSION_REGISTER, VERSION_REGISTER);
+                final byte[] data = i2cManager.getData(deviceName, 19);
+                final String version = new String(data, StandardCharsets.UTF_8);
+                log.info("ARIG Vacuum Controller version {}, {} pompes", version, NB_PUMPS);
+            } catch (I2CException e) {
+                log.error("Erreur lors de la récupération de la version de la carte ARIG Vacuum controller");
+            }
+            return null;
+        });
     }
 
     protected void sendToController() {
-        int nbTry = 0;
-        do {
-            try {
-                byte data = (byte) (states[0].getValue() + (states[1].getValue() << 2)
-                        + (states[2].getValue() << 4) + (states[3].getValue() << 6));
-                i2cManager.sendData(deviceName, WRITE_PUMPS_MODE, data);
-            } catch (I2CException e) {
-                log.error("Erreur de la modification du mode des pompes (essai {}/3) : {}", nbTry, e.toString());
-                if (nbTry < 2) {
-                    ThreadUtils.sleep(1);
+        execWithLock("sendToController", () -> {
+            int nbTry = 0;
+            do {
+                try {
+                    byte data = (byte) (states[0].getValue() + (states[1].getValue() << 2)
+                            + (states[2].getValue() << 4) + (states[3].getValue() << 6));
+                    i2cManager.sendData(deviceName, WRITE_PUMPS_MODE, data);
+                } catch (I2CException e) {
+                    log.error("Erreur de la modification du mode des pompes (essai {}/3) : {}", nbTry, e.toString());
+                    if (nbTry < 2) {
+                        ThreadUtils.sleep(1);
+                    }
                 }
-            }
-        } while (nbTry++ < 3);
+            } while (nbTry++ < 3);
+            return null;
+        });
     }
 
     protected byte[] readFromController(byte register, int size) {
-        int nbTry = 0;
-        do {
-            try {
-                i2cManager.sendData(deviceName, register, register);
-                return i2cManager.getData(deviceName, size);
-            } catch (I2CException e) {
-                log.error("Erreur lors de la récupération du registre {} (essai {}/3) : {}", register, nbTry, e.toString());
-                if (nbTry < 2) {
-                    ThreadUtils.sleep(1);
+        return execWithLock("readFromController", () -> {
+            int nbTry = 0;
+            do {
+                try {
+                    i2cManager.sendData(deviceName, register, register);
+                    return i2cManager.getData(deviceName, size);
+                } catch (I2CException e) {
+                    log.error("Erreur lors de la récupération du registre {} (essai {}/3) : {}", register, nbTry, e.toString());
+                    if (nbTry < 2) {
+                        ThreadUtils.sleep(1);
+                    }
                 }
-            }
-        } while (nbTry++ < 3);
-        return new byte[size];
+            } while (nbTry++ < 3);
+            return new byte[size];
+        });
     }
 
+    private <T> T execWithLock(String action, Callable<T> callable) {
+        try {
+            if (accessLock.tryLock(20, TimeUnit.MILLISECONDS)) {
+                try {
+                    return callable.call();
+                } finally {
+                    accessLock.unlock();
+                }
+            }
+        } catch (InterruptedException e) {
+            log.warn("Failed locking ARIGVacuumController {} : {}", action, e.toString());
+            throw new RuntimeException("Could not obtain an access-lock!", e);
+        } catch (Exception e) { // unexpected exceptions
+            log.error("Unexpected exception while executing {} : {}", action, e.toString());
+            throw new RuntimeException(e);
+        }
+        throw new RuntimeException("Could not obtain an access-lock!");
+    }
 }
