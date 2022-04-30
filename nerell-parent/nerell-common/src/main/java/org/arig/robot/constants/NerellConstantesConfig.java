7a88403a6a111653bfc7b7abf583b3989b91e948
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2022-04-30 11:42:50 +0200

    Fix #20 : Réglage odométrie de Nerell

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/constants/NerellConstantesConfig.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/constants/NerellConstantesConfig.java
@@ -32,15 +32,27 @@ public interface NerellConstantesConfig {
     // Configuration des convertions //
     // ----------------------------- //
 
-    double entraxe = 280;
-    //double entraxe = 280.0665022872784; // 1 tours
-    //double entraxe = 280.3703418872723; // 10 tours
+    //double entraxe = 280; // Mesuré
+    //double entraxe = 279.8739932288727; // 5 tours (1: 280.3958334999998, 2: 279.8739932288727)
+    //double entraxe = 279.7487166907122; // 50 tours (1: 280.1491881867973, 2: 279.7487166907122)
+
+    // 29/04/2022
+    //double entraxe = ; // 1 tour (1: 280.11787990374694, 2: 279.8743841087656, M: 279.9961320062563)
+    //double entraxe = ; // 3 tours (1: 280.1538206903572, 2: 279.90520802694107, M: 280.0295143586491)
+    //double entraxe = ; // 5 tours (1: 280.1739572404326, 2: 279.9955970798136, M: 280.0847771601231)
+    //double entraxe = ; // 10 tours (1: 280.1866799431399, 2: 279.93264432000274, M: 280.0596621315713)
+    //double entraxe = ; // 20 tours (1: 280.16442357229266, 2: 279.9582320272419, M: 280.06132779976724)
+    //double entraxe = ; // 30 tours (1: 280.1720458960358, 2: 279.9663528029184, M: 280.0691993494771)
+    //double entraxe = ; // 50 tours (1: 280.1867535816509, 2: 279.9567559204513, M: 280.0717547510511)
+    double entraxe = 279.941310612305; // Moyenne des 2
+
 
     // Diam 47mm => périmètre 147.655 mm
     // 4096 p => 147.65485471872 mm : 4096 / 147.65485471872000000 = 27.74036795337890000
     //double countPerMm = 27.74036795337890000; // Théorique
     //double countPerMm = 27.536394659604056; // Manuel 2700 -> mesure 2720 depuis le théorique
-    double countPerMm = 27.387010963439838; // Manuel 2750 -> mesure 2765 depuis le manuel 2720
+    //double countPerMm = 27.387010963439838; // Manuel 2750 -> mesure 2765 depuis le manuel 2720
+    double countPerMm = 27.37705567046876; // Manuel 2750 -> mesure 2751 depuis le manuel 2765
 
     // Entraxe 280mm => périmètre 879.64594300514200000 mm (1 roue)
     // 879.64594300514200000 mm => 180° : 879.64594300514200000 * 27.74036795337890000 / 180 =
@@ -51,7 +63,7 @@ public interface NerellConstantesConfig {
     //double countPerDeg = 133.75398669244305; // 50 tours
 
     //double coefCodeurDroit = (0.9914760162438181 + 0.9901903052878885 + 0.9901004087212356) / 3;
-    double coefCodeurDroit = 0.9920508329274527; //0.990588910084314;
+    double coefCodeurDroit = 0.9928107224103835; //0.9920508329274527; //0.990588910084314;
     double coefCodeurGauche = 1;
 
     // ------------------------ //
