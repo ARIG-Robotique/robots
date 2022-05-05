044fffb6c5036ac257007c012ab31acd8146f50e
Damien Sorel <damien.sorel@sogelink.fr>
2022-05-05 21:43:50 +0200

    Revert "Fix dépendance cyclique"
    This reverts commit d4968a3866db1016060c9b96c7b8739033dd0e3a.

--- eurobot-system-lib-parent/eurobot-system-lib-bouchon/src/main/java/org/arig/robot/system/capteurs/CarreFouilleReaderBouchon.java
+++ eurobot-system-lib-parent/eurobot-system-lib-bouchon/src/main/java/org/arig/robot/system/capteurs/CarreFouilleReaderBouchon.java
@@ -3,15 +3,23 @@ package org.arig.robot.system.capteurs;
 import org.arig.robot.communication.I2CManager;
 import org.arig.robot.exception.I2CException;
 import org.arig.robot.model.CouleurCarreFouille;
+import org.arig.robot.model.EurobotStatus;
+import org.arig.robot.model.Team;
 
 public class CarreFouilleReaderBouchon extends CarreFouilleReader {
 
-    public CarreFouilleReaderBouchon(I2CManager i2cManager, String deviceName) {
+    private final EurobotStatus status;
+
+    public CarreFouilleReaderBouchon(final I2CManager i2cManager, final String deviceName, final EurobotStatus status) {
         super(i2cManager, deviceName);
+        this.status = status;
     }
 
     @Override
     public CouleurCarreFouille readCarreFouille() throws I2CException {
-        return CouleurCarreFouille.INCONNU;
+        if (status.team() == null) {
+            return CouleurCarreFouille.INCONNU;
+        }
+        return status.team() == Team.JAUNE ? CouleurCarreFouille.JAUNE : CouleurCarreFouille.VIOLET;
     }
 }
