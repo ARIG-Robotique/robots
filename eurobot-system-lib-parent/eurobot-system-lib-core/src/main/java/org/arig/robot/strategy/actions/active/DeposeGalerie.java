83bb63d5a436f846e93e24a36dcf9924bab98f5d
Damien Sorel <damien.sorel@sogelink.fr>
2022-05-13 10:50:52 +0200

    Revert "Evitement dépose galerie"
    This reverts commit 471fdf45813d7f41efe4b30983bd653277159c87.

--- eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/strategy/actions/active/DeposeGalerie.java
+++ eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/strategy/actions/active/DeposeGalerie.java
@@ -119,11 +119,9 @@ public class DeposeGalerie extends AbstractEurobotAction {
                 }
 
                 // On se place à la position permettant de tourner le robot
-                rs.enableAvoidance();
+                rs.disableAvoidance();
                 mv.setVitesse(config.vitesse(), config.vitesseOrientation());
                 mv.gotoPoint(entryPoint.getX(), yRefBordure - OFFSET_Y_REF_POUR_ROTATION, GotoOption.SANS_ORIENTATION);
-
-                rs.disableAvoidance();
 
                 CouleurEchantillon echantillonDepose = null;
                 if (pos.etage() == Galerie.Etage.BAS) {
