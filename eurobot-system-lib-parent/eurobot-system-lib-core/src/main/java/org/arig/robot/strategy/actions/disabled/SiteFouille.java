5baa1707a0acdb49ec9850259e6db17c8004bf74
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2022-05-24 11:32:03 +0200

    Au revoir site de fouille

--- eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/strategy/actions/disabled/SiteFouille.java
+++ eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/strategy/actions/disabled/SiteFouille.java
@@ -1,4 +1,4 @@
-package org.arig.robot.strategy.actions.active;
+package org.arig.robot.strategy.actions.disabled;
 
 import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.constants.EurobotConfig;
