bc5b09c00c1bd613ded392092f7dbf2c4ebf6990
Damien Sorel <damien.sorel@sogelink.fr>
2022-05-25 23:39:11 +0200

    Supprime les masques échantillons en fin de match

--- eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/system/pathfinding/GameMultiPathFinderImpl.java
+++ eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/system/pathfinding/GameMultiPathFinderImpl.java
@@ -26,20 +26,22 @@ public class GameMultiPathFinderImpl extends MultiPathFinderImpl {
     @Override
     public void setObstacles(final List<Shape> obstacles) {
 
-        // ajout des echantillons
-        for (Echantillon echantillon : rs.echantillons()) {
-            if (echantillon.isBlocking()) {
-                obstacles.add(tableUtils.createPolygonObstacle(echantillon, EurobotConfig.PATHFINDER_ECHANTILLON_SIZE));
+        if (rs.getRemainingTime() > 10000) {
+            // ajout des echantillons
+            for (Echantillon echantillon : rs.echantillons()) {
+                if (echantillon.isBlocking()) {
+                    obstacles.add(tableUtils.createPolygonObstacle(echantillon, EurobotConfig.PATHFINDER_ECHANTILLON_SIZE));
+                }
             }
-        }
 
-        // ajout des sites de fouille
-        if (!StringUtils.startsWith(rs.currentAction(), EurobotConfig.ACTION_RETOUR_SITE_DE_FOUILLE_PREFIX) && rs.getRemainingTime() > 10000) {
-            if (!rs.siteDeFouillePris() && rs.team() == Team.JAUNE || !rs.siteDeFouilleAdversePris() && rs.team() == Team.VIOLET) {
-                obstacles.add(tableUtils.createPolygonObstacle(new Point(975, 625), EurobotConfig.PATHFINDER_SITE_FOUILLE_SIZE));
-            }
-            if (!rs.siteDeFouillePris() && rs.team() == Team.VIOLET || !rs.siteDeFouilleAdversePris() && rs.team() == Team.JAUNE) {
-                obstacles.add(tableUtils.createPolygonObstacle(new Point(2025, 625), EurobotConfig.PATHFINDER_SITE_FOUILLE_SIZE));
+            // ajout des sites de fouille
+            if (!StringUtils.startsWith(rs.currentAction(), EurobotConfig.ACTION_RETOUR_SITE_DE_FOUILLE_PREFIX)) {
+                if (!rs.siteDeFouillePris() && rs.team() == Team.JAUNE || !rs.siteDeFouilleAdversePris() && rs.team() == Team.VIOLET) {
+                    obstacles.add(tableUtils.createPolygonObstacle(new Point(975, 625), EurobotConfig.PATHFINDER_SITE_FOUILLE_SIZE));
+                }
+                if (!rs.siteDeFouillePris() && rs.team() == Team.VIOLET || !rs.siteDeFouilleAdversePris() && rs.team() == Team.JAUNE) {
+                    obstacles.add(tableUtils.createPolygonObstacle(new Point(2025, 625), EurobotConfig.PATHFINDER_SITE_FOUILLE_SIZE));
+                }
             }
         }
 
