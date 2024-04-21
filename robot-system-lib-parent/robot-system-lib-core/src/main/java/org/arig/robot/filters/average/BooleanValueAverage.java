bbedb52b9ce4bced63fe82c7782b58bf9941743c
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-04-21 15:22:08 +0200

    Boolean average filter

---
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/filters/average/BooleanValueAverage.java
@@ -0,0 +1,48 @@
+package org.arig.robot.filters.average;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.util.function.BinaryOperator;
+
+/**
+ * @author gdepuille on 08/05/15.
+ */
+public class BooleanValueAverage implements Average<Boolean> {
+
+    private final DoubleValueAverage delegate;
+
+    @Getter
+    @Accessors(fluent = true)
+    private Boolean lastResult;
+
+    public BooleanValueAverage() {
+        this(3);
+    }
+
+    public BooleanValueAverage(int limit) {
+        delegate = new DoubleValueAverage(limit);
+    }
+
+    @Override
+    public void setNbValues(int nbValues) {
+        delegate.setNbValues(nbValues);
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public void reset() {
+        delegate.reset();
+    }
+
+    @Override
+    public Boolean filter(Boolean value) {
+        Double result = delegate.filter((double) (value ? 1 : 0));
+        lastResult = (result >= 0.5);
+        return lastResult;
+    }
+}
---
+++ robot-system-lib-parent/robot-system-lib-core/src/test/java/org/arig/robot/filters/average/BooleanValueAverageTest.java
@@ -0,0 +1,41 @@
+package org.arig.robot.filters.average;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author gdepuille on 08/05/15.
+ */
+@ExtendWith(SpringExtension.class)
+public class BooleanValueAverageTest {
+
+    @Test
+    public void testFullTrueAverage() {
+        BooleanValueAverage avg = new BooleanValueAverage();
+        Assertions.assertTrue(avg.filter(true));
+        Assertions.assertTrue(avg.filter(true));
+        Assertions.assertTrue(avg.filter(true));
+    }
+
+    @Test
+    public void testFullFalseAverage() {
+        BooleanValueAverage avg = new BooleanValueAverage();
+        Assertions.assertFalse(avg.filter(false));
+        Assertions.assertFalse(avg.filter(false));
+        Assertions.assertFalse(avg.filter(false));
+    }
+
+    @Test
+    public void testIntermitentAverage() {
+        BooleanValueAverage avg = new BooleanValueAverage();
+        Assertions.assertFalse(avg.filter(false));
+        Assertions.assertTrue(avg.filter(true));
+        Assertions.assertTrue(avg.filter(true));
+        Assertions.assertTrue(avg.filter(true));
+        Assertions.assertTrue(avg.filter(false));
+        Assertions.assertFalse(avg.filter(false));
+        Assertions.assertFalse(avg.filter(false));
+    }
+}
