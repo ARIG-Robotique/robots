ff707734e8bfdf11e60ee3622f6ad2f140f1f368
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-04-23 12:13:39 +0200

    Fix pre-commit

--- robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/model/enums/TypeCalage.java
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/model/enums/TypeCalage.java
@@ -3,10 +3,10 @@ package org.arig.robot.model.enums;
 public enum TypeCalage {
     AVANT,
     ARRIERE,
-    
+
     FORCE,
     TEMPO,
-    
+
     PRISE_PRODUIT_AVANT,
     PRISE_PRODUIT_ARRIERE,
     PRISE_ELECTROAIMANT
