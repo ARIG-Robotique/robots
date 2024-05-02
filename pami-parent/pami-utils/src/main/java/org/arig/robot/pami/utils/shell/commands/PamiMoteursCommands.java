a213e5698ffbfcb72dc21e5ba9a66be48f623e82
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-05-02 18:10:26 +0200

    Check motors limit for robots

--- pami-parent/pami-utils/src/main/java/org/arig/robot/pami/utils/shell/commands/PamiMoteursCommands.java
+++ pami-parent/pami-utils/src/main/java/org/arig/robot/pami/utils/shell/commands/PamiMoteursCommands.java
@@ -1,9 +1,11 @@
 package org.arig.robot.pami.utils.shell.commands;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.arig.robot.model.AbstractRobotStatus;
 import org.arig.robot.services.AbstractEnergyService;
 import org.arig.robot.services.PamiIOService;
+import org.arig.robot.system.encoders.Abstract2WheelsEncoders;
 import org.arig.robot.system.motors.AbstractPropulsionsMotors;
 import org.arig.robot.utils.ThreadUtils;
 import org.springframework.shell.Availability;
@@ -12,6 +14,7 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellMethodAvailability;
 
+@Slf4j
 @ShellComponent
 @ShellCommandGroup("Moteurs")
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ public class PamiMoteursCommands {
     private final PamiIOService ioService;
     private final AbstractEnergyService energyService;
     private final AbstractPropulsionsMotors propulsionsMotors;
+    private final Abstract2WheelsEncoders wheelsEncoders;
 
     public Availability alimentationOk() {
         return ioService.auOk() && energyService.checkMoteurs()
@@ -55,6 +59,67 @@ public class PamiMoteursCommands {
         ThreadUtils.sleep(wait);
 
         propulsionsMotors.generateMouvement(propulsionsMotors.getStopSpeed(), propulsionsMotors.getStopSpeed());
+        rs.disableCapture();
+    }
+
+    @ShellMethodAvailability("alimentationOk")
+    @ShellMethod("Reglages des limites de moteurs")
+    public void limitMotors() {
+        int limitRightUp = -1;
+        int limitRightDown = -1;
+        int limitLeftUp = -1;
+        int limitLeftDown = -1;
+
+        int cmdRight = 0;
+        int cmdLeft = 0;
+
+        int cycle = 0;
+
+        rs.enableCapture();
+
+        wheelsEncoders.reset();
+        do {
+            propulsionsMotors.generateMouvement(cmdLeft, cmdRight);
+            log.info("Cycle UP {} : L {} / R {}", cycle, cmdLeft, cmdRight);
+            ThreadUtils.sleep(100);
+            wheelsEncoders.lectureValeurs();
+            if (wheelsEncoders.getGauche() > 1) {
+                limitLeftUp = cmdLeft;
+            } else {
+                cmdLeft += 10;
+            }
+            if (wheelsEncoders.getDroit() > 1) {
+                limitRightUp = cmdRight;
+            } else {
+                cmdRight += 10;
+            }
+        } while (limitLeftUp == -1 || limitRightUp == -1);
+
+        wheelsEncoders.reset();
+        do {
+            propulsionsMotors.generateMouvement(cmdLeft, cmdRight);
+            log.info("Cycle DOWN {} : L {} / R {}", cycle, cmdLeft, cmdRight);
+            ThreadUtils.sleep(100);
+            wheelsEncoders.lectureValeurs();
+            if (wheelsEncoders.getGauche() < 1) {
+                limitLeftDown = cmdLeft;
+            } else {
+                cmdLeft -= 10;
+            }
+            if (wheelsEncoders.getDroit() < 1) {
+                limitRightDown = cmdRight;
+            } else {
+                cmdRight -= 10;
+            }
+        } while (limitLeftDown == -1 || limitRightDown == -1);
+
+        log.info("Limit Right UP : {} / Limit Right Down : {}", limitRightUp, limitRightDown);
+        log.info("Limit Left UP  : {} / Limit Left Down  : {}", limitLeftUp, limitLeftDown);
+
+        propulsionsMotors.stopAll();
+        ThreadUtils.sleep(500);
+        wheelsEncoders.reset();
+
         rs.disableCapture();
     }
 }
