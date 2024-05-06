03a9516a3360b8c56781b087835ba7d6083918e3
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-05-06 16:15:27 +0200

    Read Pami IO GP2D raw data on 2 bytes

--- robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/capteurs/i2c/ARIG2024IoPamiSensors.java
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/capteurs/i2c/ARIG2024IoPamiSensors.java
@@ -17,20 +17,20 @@ public class ARIG2024IoPamiSensors {
     private boolean arriereGauche;
     private boolean arriereDroite;
 
-    private byte gp2dGauche = -1;
-    private byte gp2dCentre = -1;
-    private byte gp2dDroite = -1;
+    private short gp2dGauche = -1;
+    private short gp2dCentre = -1;
+    private short gp2dDroite = -1;
 
     public void refreshSensors() {
         try {
             log.debug("Refresh sensors");
 
-            final byte[] data = i2cManager.getData(deviceName, 4);
+            final byte[] data = i2cManager.getData(deviceName, 7);
             arriereGauche = (data[0] & 0x01) == 0x01;
             arriereDroite = (data[0] & 0x02) == 0x02;
-            gp2dGauche = data[1];
-            gp2dCentre = data[2];
-            gp2dDroite = data[3];
+            gp2dGauche = ((short) ((data[1] << 8) + (data[2] & 0xFF)));
+            gp2dCentre = ((short) ((data[3] << 8) + (data[4] & 0xFF)));
+            gp2dDroite = ((short) ((data[5] << 8) + (data[6] & 0xFF)));
 
             log.debug("IO Pami sensors : In1 = {} ; In2 = {} ; GP2D1 = {} ; GP2D2 = {} ; GP2D3 = {}",
                 arriereGauche, arriereDroite, gp2dGauche, gp2dCentre, gp2dDroite);
