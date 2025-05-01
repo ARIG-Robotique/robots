48c22dd7f71e393e20984d7c2e565554784f6763
Gregory DEPUILLE <gregory.depuille@gmail.com>
2025-05-02 01:42:02 +0200

    Updates servo IDs
    Updates the IDs assigned to the various servos on the robot.
    This reflects the new physical arrangement and wiring of the servos.

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/services/NerellRobotServosService.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/services/NerellRobotServosService.java
@@ -7,25 +7,25 @@ import org.arig.robot.system.servos.i2c.SD21Servos;
 @Slf4j
 public class NerellRobotServosService extends AbstractCommonRobotServosService {
 
-    public static final byte TIROIR_AVANT_ID = 1;
-    public static final byte BEC_AVANT_ID = 2;
-    public static final byte ASCENSEUR_AVANT_ID = 3;
-    public static final byte PINCE_AVANT_GAUCHE_ID = 4;
-    public static final byte DOIGT_AVANT_GAUCHE_ID = 5;
-    public static final byte PINCE_AVANT_DROIT_ID = 6;
-    public static final byte DOIGT_AVANT_DROIT_ID = 7;
-    public static final byte BLOCK_COLONNE_AVANT_GAUCHE_ID = 8;
-    public static final byte BLOCK_COLONNE_AVANT_DROIT_ID = 9;
+    public static final byte TIROIR_AVANT_ID = 17;
+    public static final byte BEC_AVANT_ID = 21;
+    public static final byte ASCENSEUR_AVANT_ID = 16;
+    public static final byte PINCE_AVANT_GAUCHE_ID = 12;
+    public static final byte DOIGT_AVANT_GAUCHE_ID = 15;
+    public static final byte PINCE_AVANT_DROIT_ID = 14;
+    public static final byte DOIGT_AVANT_DROIT_ID = 13;
+    public static final byte BLOCK_COLONNE_AVANT_GAUCHE_ID = 4;
+    public static final byte BLOCK_COLONNE_AVANT_DROIT_ID = 2;
 
-    public static final byte TIROIR_ARRIERE_ID = 10;
-    public static final byte BEC_ARRIERE_ID = 11;
-    public static final byte ASCENSEUR_ARRIERE_ID = 12;
-    public static final byte PINCE_ARRIERE_GAUCHE_ID = 13;
-    public static final byte DOIGT_ARRIERE_GAUCHE_ID = 14;
-    public static final byte PINCE_ARRIERE_DROIT_ID = 15;
-    public static final byte DOIGT_ARRIERE_DROIT_ID = 16;
-    public static final byte BLOCK_COLONNE_ARRIERE_GAUCHE_ID = 17;
-    public static final byte BLOCK_COLONNE_ARRIERE_DROIT_ID = 18;
+    public static final byte TIROIR_ARRIERE_ID = 8;
+    public static final byte BEC_ARRIERE_ID = 7;
+    public static final byte ASCENSEUR_ARRIERE_ID = 9;
+    public static final byte PINCE_ARRIERE_GAUCHE_ID = 10;
+    public static final byte DOIGT_ARRIERE_GAUCHE_ID = 6;
+    public static final byte PINCE_ARRIERE_DROIT_ID = 11;
+    public static final byte DOIGT_ARRIERE_DROIT_ID = 5;
+    public static final byte BLOCK_COLONNE_ARRIERE_GAUCHE_ID = 1;
+    public static final byte BLOCK_COLONNE_ARRIERE_DROIT_ID = 3;
 
     public NerellRobotServosService(SD21Servos servos) {
         super(servos);
