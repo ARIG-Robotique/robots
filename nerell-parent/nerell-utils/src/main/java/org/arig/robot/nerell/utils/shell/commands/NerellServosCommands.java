9c6e27e9e716e20af9b1a420335c4a6aef8de0ee
Gregory DEPUILLE <gregory.depuille@gmail.com>
2025-05-05 13:51:03 +0200

    Adds initial servo configurations
    Adds initial configurations for servo commands, including pre-positioning of related components before the main loop and a preparation call after. This ensures that the servos are in a known state before starting their automated sequences.

--- nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/commands/NerellServosCommands.java
+++ nerell-parent/nerell-utils/src/main/java/org/arig/robot/nerell/utils/shell/commands/NerellServosCommands.java
@@ -47,121 +47,159 @@ public class NerellServosCommands {
 
     @ShellMethod("Configuration attente pince avant")
     public void configWaitPinceAvant(int wait) {
+        servosService.groupeDoigtsAvantFerme(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.groupePincesAvantOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.groupePincesAvantFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente pince arriere")
     public void configWaitPinceArriere(int wait) {
+        servosService.groupeDoigtsArriereFerme(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.groupePincesArriereOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.groupePincesArriereFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente doigts avant")
     public void configWaitDoigtsAvant(int wait) {
+        servosService.groupePincesAvantOuvert(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.groupeDoigtsAvantOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.groupeDoigtsAvantFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente doigts arriere")
     public void configWaitDoigtsArriere(int wait) {
+        servosService.groupePincesArriereOuvert(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.groupeDoigtsArriereOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.groupeDoigtsArriereFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente block colonne avant")
     public void configWaitBlockColonneAvant(int wait) {
+        servosService.groupePincesAvantOuvert(false);
+        servosService.ascenseurAvantHaut(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.groupeBlockColonneAvantOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.groupeBlockColonneAvantFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente block colonne arriere")
     public void configWaitBlockColonneArriere(int wait) {
+        servosService.groupePincesArriereOuvert(false);
+        servosService.ascenseurArriereHaut(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.groupeBlockColonneArriereOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.groupeBlockColonneArriereFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente ascenseur avant")
     public void configWaitAscenseurAvant(int wait) {
+        servosService.groupePincesAvantOuvert(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.ascenseurAvantHaut(false);
             ThreadUtils.sleep(wait);
             servosService.ascenseurAvantBas(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente ascenseur arriere")
     public void configWaitAscenseurArriere(int wait) {
+        servosService.groupePincesArriereOuvert(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.ascenseurArriereHaut(false);
             ThreadUtils.sleep(wait);
             servosService.ascenseurArriereBas(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente tiroir avant")
     public void configWaitTiroirAvant(int wait) {
+        servosService.becAvantFerme(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.tiroirAvantOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.tiroirAvantStock(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente tiroir arriere")
     public void configWaitTiroirArriere(int wait) {
+        servosService.becArriereFerme(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.tiroirArriereOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.tiroirArriereStock(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente bec avant")
     public void configWaitBecAvant(int wait) {
+        servosService.tiroirAvantOuvert(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.becAvantOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.becAvantFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 
     @ShellMethod("Configuration attente bec arriere")
     public void configWaitBecArriere(int wait) {
+        servosService.tiroirArriereOuvert(true);
         for (int i = 0; i < nbLoop; i++) {
             servosService.becArriereOuvert(false);
             ThreadUtils.sleep(wait);
             servosService.becArriereFerme(false);
             ThreadUtils.sleep(wait);
         }
+
+        preparation();
     }
 }
