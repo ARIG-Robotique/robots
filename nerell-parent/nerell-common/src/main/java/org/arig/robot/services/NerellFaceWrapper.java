70f03accf418837cfafabe2e5e5a55d55aa4ae67
Gregory DEPUILLE <gregory.depuille@gmail.com>
2025-05-07 23:53:45 +0200

    Adds method to retrieve a face for construction
    Implements a method to determine the appropriate face
    (front or rear) for construction based on the required
    number of floors.  The method prioritizes faces with
    the exact required number of constructible floors but
    falls back to any face with constructible floors if an
    exact match isn't found. Logs a warning if no suitable
    face is available.

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/services/NerellFaceWrapper.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/services/NerellFaceWrapper.java
@@ -28,6 +28,31 @@ public class NerellFaceWrapper {
     }
   }
 
+  public Face getConstructionFace(int nbEtageRequis) {
+    if (robotStatus.faceAvant().isEmpty() && robotStatus.faceArriere().isEmpty()) {
+      // Aucune face n'est rempli pour faire une construction
+      return null;
+    }
+
+    // Récupération de la face avec le nombre d'étage requis s'il y en a une
+    if (robotStatus.faceAvant().nbEtageConstructible() == nbEtageRequis) {
+      return Face.AVANT;
+    } else if (robotStatus.faceArriere().nbEtageConstructible() == nbEtageRequis) {
+      return Face.ARRIERE;
+    }
+
+    // Sinon on regarde si on peut construire un étage avec une des faces
+    if (robotStatus.faceAvant().nbEtageConstructible() > 0) {
+      return Face.AVANT;
+    } else if (robotStatus.faceArriere().nbEtageConstructible() > 0) {
+      return Face.ARRIERE;
+    }
+
+    // Rien ne correspond, c'est louche
+    log.warn("Aucune face ne correspond à la construction pour {} étage(s). Bizarre !", nbEtageRequis);
+    return null;
+  }
+
   public AbstractNerellFaceService getFaceService(final Face face ) {
     return switch (face) {
       case AVANT -> faceAvantService;
