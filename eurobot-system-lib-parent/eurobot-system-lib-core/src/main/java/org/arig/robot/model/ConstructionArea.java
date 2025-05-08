958990c2644e35964b0dcf57e5c3ce8294d5c766
Gregory DEPUILLE <gregory.depuille@gmail.com>
2025-05-08 22:32:58 +0200

    Adds ConstructionArea model
    Introduces the `ConstructionArea` model with logic for managing and scoring construction areas.
    
    This includes methods for adding, removing, and querying available slots, along with unit tests.
    The implementation uses enums for representing the rang and etage, enhancing code readability and maintainability.

--- eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/model/ConstructionArea.java
+++ eurobot-system-lib-parent/eurobot-system-lib-core/src/main/java/org/arig/robot/model/ConstructionArea.java
@@ -1,6 +1,8 @@
 package org.arig.robot.model;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +17,47 @@ public class ConstructionArea {
 
     private final String name;
     private final byte nbRang;
+    @Getter(AccessLevel.NONE)
     private final byte nbEtage = 3;
     private final boolean[][] data;
+
+    @Accessors(fluent = true)
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public enum Rang {
+        RANG_1((byte) 0),
+        RANG_2((byte) 1),
+        RANG_3((byte) 2);
+
+        private final byte idx;
+
+        private static Rang fromIdx(byte rang) {
+            return switch (rang) {
+                case 0 -> RANG_1;
+                case 1 -> RANG_2;
+                case 2 -> RANG_3;
+                default -> throw new IllegalArgumentException("Invalid rang index: " + rang);
+            };
+        }
+    }
+
+    @Accessors(fluent = true)
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public enum Etage {
+        ETAGE_1((byte) 0),
+        ETAGE_2((byte) 1),
+        ETAGE_3((byte) 2);
+
+        private final byte idx;
+
+        private static Etage fromIdx(byte etage) {
+            return switch (etage) {
+                case 0 -> ETAGE_1;
+                case 1 -> ETAGE_2;
+                case 2 -> ETAGE_3;
+                default -> throw new IllegalArgumentException("Invalid etage index: " + etage);
+            };
+        }
+    }
 
     public ConstructionArea(String name) {
         this(name, (byte) 1);
@@ -45,21 +86,97 @@ public class ConstructionArea {
         return score;
     }
 
-    public void addGradin(int rang, int etage) {
-        checkPosition(rang, etage);
-        log.info("[RS] Ajout tribune rang {} etage {}", rang, etage);
-        data[rang][etage] = true;
+    public void addGradin(Rang rang, Etage etage) {
+        log.info("Ajout tribune rang {} etage {}", rang, etage);
+        data[rang.idx][etage.idx] = true;
     }
 
-    public void removeGradin(int rang, int etage) {
-        checkPosition(rang, etage);
-        log.info("[RS] Remove tribune rang {} etage {}", rang, etage);
-        data[rang][etage] = false;
+    public void removeGradin(Rang rang, Etage etage) {
+        log.info("Remove tribune rang {} etage {}", rang, etage);
+        data[rang.idx][etage.idx] = false;
+    }
+
+    public Rang getFirstConstructibleRang(boolean limit2Etage) {
+        byte nbEtage = limit2Etage ? 2 : this.nbEtage;
+
+        // Récupération du premier rang non vide
+        Rang firstRangNonVide = null;
+        boolean firstRangNonVideConstructible = false;
+        if (nbRang > 2 && !rangEmpty(Rang.RANG_3, nbEtage)) {
+            firstRangNonVide = Rang.RANG_3;
+            firstRangNonVideConstructible = getFirstConstructibleEtage(Rang.RANG_3, limit2Etage) != null;
+        } else if (nbRang > 1 && !rangEmpty(Rang.RANG_2, nbEtage)) {
+            firstRangNonVide = Rang.RANG_2;
+            firstRangNonVideConstructible = getFirstConstructibleEtage(Rang.RANG_2, limit2Etage) != null;
+        } else if (!rangEmpty(Rang.RANG_1, nbEtage)) {
+            firstRangNonVide = Rang.RANG_1;
+            firstRangNonVideConstructible = getFirstConstructibleEtage(Rang.RANG_1, limit2Etage) != null;
+        }
+
+        Rang firstRangVide = null;
+        boolean firstRangVideConstructible = false;
+        if (rangEmpty(Rang.RANG_1, nbEtage)) {
+            firstRangVide = Rang.RANG_1;
+            firstRangVideConstructible = getFirstConstructibleEtage(Rang.RANG_1, limit2Etage) != null;
+        } else if (nbRang > 1 && rangEmpty(Rang.RANG_2, nbEtage)) {
+            firstRangVide = Rang.RANG_2;
+            firstRangVideConstructible = getFirstConstructibleEtage(Rang.RANG_2, limit2Etage) != null;
+        } else if (nbRang > 2 && rangEmpty(Rang.RANG_3, nbEtage)) {
+            firstRangVide = Rang.RANG_3;
+            firstRangVideConstructible = getFirstConstructibleEtage(Rang.RANG_3, limit2Etage) != null;
+        }
+
+        if (firstRangNonVide == null && firstRangVide == null) {
+            // Aucun rang constructible
+            return null;
+        }
+        if (firstRangVide == null && firstRangNonVideConstructible) {
+            // Rang non vide constructible
+            return firstRangNonVide;
+        }
+        if (firstRangNonVide == null && firstRangVideConstructible) {
+            // Rang vide constructible
+            return firstRangVide;
+        }
+
+        // Si les deux rang vide et non vide sont les mêmes -> bizarrerie
+        if (firstRangVide == firstRangNonVide && firstRangVideConstructible) {
+            return firstRangVide;
+        }
+
+        if (firstRangVide != null && firstRangNonVide != null) {
+            // Cas nominal
+            if (firstRangVide.idx > firstRangNonVide.idx) {
+                return firstRangNonVideConstructible ? firstRangNonVide : firstRangVide;
+            }
+
+            // Si le premier rang vide est inférieur au premier rang non vide -> pb
+            if (firstRangVide.idx < firstRangNonVide.idx) {
+                if (firstRangNonVideConstructible) {
+                    return firstRangNonVide;
+                }
+            }
+        }
+
+        // Aucun rang constructible
+        return null;
+    }
+
+    public Etage getFirstConstructibleEtage(Rang rang, boolean limit2Etage) {
+        if(rang != null) {
+            byte nbEtage = limit2Etage ? 2 : this.nbEtage;
+            for (byte etage = 0; etage < nbEtage; etage++) {
+                if (!data[rang.idx][etage]) {
+                    return Etage.fromIdx(etage);
+                }
+            }
+        }
+        return null;
     }
 
     public boolean isEmpty() {
-        for (int row = 0; row < nbRang; row++) {
-            for (int col = 0; col < nbEtage; col++) {
+        for (byte row = 0; row < nbRang; row++) {
+            for (byte col = 0; col < nbEtage; col++) {
                 if (data[row][col]) {
                     return false;
                 }
@@ -73,12 +190,20 @@ public class ConstructionArea {
         return Arrays.deepToString(data);
     }
 
-    private void checkPosition(int rang, int etage) {
-        if (rang < 0 || rang >= nbRang) {
-            throw new IllegalArgumentException("Rang " + rang + " out of bounds");
+    void clean() {
+        for (byte row = 0; row < nbRang; row++) {
+            for (byte col = 0; col < nbEtage; col++) {
+                data[row][col] = false;
+            }
         }
-        if (etage < 0 || etage >= nbEtage) {
-            throw new IllegalArgumentException("Etage " + etage + " out of bounds");
+    }
+
+    private boolean rangEmpty(Rang rang, byte nbEtage) {
+        for (byte etage = 0; etage < nbEtage; etage++) {
+            if (data[rang.idx][etage]) {
+                return false;
+            }
         }
+        return true;
     }
 }
--- eurobot-system-lib-parent/eurobot-system-lib-core/src/test/java/org/arig/robot/model/ConstructionAreaTest.java
+++ eurobot-system-lib-parent/eurobot-system-lib-core/src/test/java/org/arig/robot/model/ConstructionAreaTest.java
@@ -6,44 +6,118 @@ import org.junit.jupiter.api.Test;
 public class ConstructionAreaTest {
 
     @Test
-    public void testGrandeZone() {
+    public void testCalculScore() {
         ConstructionArea area = new ConstructionArea("zone", (byte) 3);
 
         Assertions.assertTrue(area.isEmpty());
         Assertions.assertEquals(0, area.score());
 
-        area.addGradin(0, 0);
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_1);
         Assertions.assertFalse(area.isEmpty());
         Assertions.assertEquals(4, area.score());
 
-        area.addGradin(1, 0);
+        area.addGradin(ConstructionArea.Rang.RANG_2, ConstructionArea.Etage.ETAGE_1);
         Assertions.assertEquals(8, area.score());
 
-        area.addGradin(2, 0);
+        area.addGradin(ConstructionArea.Rang.RANG_3, ConstructionArea.Etage.ETAGE_1);
         Assertions.assertEquals(12, area.score());
 
-        area.removeGradin(2, 0);
+        area.removeGradin(ConstructionArea.Rang.RANG_3, ConstructionArea.Etage.ETAGE_1);
         Assertions.assertEquals(8, area.score());
-        area.addGradin(1, 1);
+        area.addGradin(ConstructionArea.Rang.RANG_2, ConstructionArea.Etage.ETAGE_2);
         Assertions.assertEquals(16, area.score());
 
-        area.removeGradin(1, 1);
-        area.removeGradin(1, 0);
+        area.removeGradin(ConstructionArea.Rang.RANG_2, ConstructionArea.Etage.ETAGE_2);
+        area.removeGradin(ConstructionArea.Rang.RANG_2, ConstructionArea.Etage.ETAGE_1);
         Assertions.assertEquals(4, area.score());
-        area.addGradin(0, 1);
-        area.addGradin(0, 2);
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_2);
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_3);
         Assertions.assertEquals(28, area.score());
     }
 
     @Test
-    public void testGrandeZoneOversize() {
-        ConstructionArea area = new ConstructionArea("zone");
+    public void testFirstRangConstructiblePetiteZoneLimit2Etage() {
+        final boolean limit2Etage = true;
+        ConstructionArea area = new ConstructionArea("petite zone test", (byte) 1);
 
-        IllegalArgumentException ex = Assertions
-            .assertThrows(IllegalArgumentException.class, () -> area.addGradin(2, 0));
-        Assertions.assertEquals("Rang 2 out of bounds", ex.getMessage());
+        // Vide
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_1, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_1, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
 
-        ex = Assertions.assertThrows(IllegalArgumentException.class, () -> area.addGradin(0, 5));
-        Assertions.assertEquals("Etage 5 out of bounds", ex.getMessage());
+        // Rang 1 etage 1
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_1);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_1, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_2, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
+
+        // Rang 1 etage 2
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_2);
+        Assertions.assertNull(area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertNull(area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
+        Assertions.assertNull(area.getFirstConstructibleEtage(null, limit2Etage));
+    }
+
+    @Test
+    public void testFirstRangConstructiblePetiteZone() {
+        final boolean limit2Etage = false;
+        ConstructionArea area = new ConstructionArea("petite zone test", (byte) 1);
+
+        // Vide
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_1, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_1, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
+
+        // Rang 1 etage 1
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_1);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_1, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_2, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
+
+        // Rang 1 etage 2
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_2);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_1, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_3, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
+
+        // Rang 1 etage 3
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_3);
+        Assertions.assertNull(area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertNull(area.getFirstConstructibleEtage(null, limit2Etage));
+    }
+
+    @Test
+    public void testFirstRangConstructibleGrandeZoneLimit2Etage() {
+        boolean limit2Etage = true;
+        ConstructionArea area = new ConstructionArea("grande zone test", (byte) 3);
+
+        // Vide
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_1, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_1, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
+
+        // Rang 1 etage 1
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_1);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_1, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_2, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_1, limit2Etage));
+
+        // Rang 1 etage 2
+        area.addGradin(ConstructionArea.Rang.RANG_1, ConstructionArea.Etage.ETAGE_2);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_2, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_1, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_2, limit2Etage));
+
+        // Rang 2 etage 1
+        area.addGradin(ConstructionArea.Rang.RANG_2, ConstructionArea.Etage.ETAGE_1);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_2, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_2, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_2, limit2Etage));
+
+        // Rang 2 etage 2
+        area.addGradin(ConstructionArea.Rang.RANG_2, ConstructionArea.Etage.ETAGE_2);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_3, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_1, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_3, limit2Etage));
+
+        // Rang 3 etage 1
+        area.addGradin(ConstructionArea.Rang.RANG_3, ConstructionArea.Etage.ETAGE_1);
+        Assertions.assertEquals(ConstructionArea.Rang.RANG_3, area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertEquals(ConstructionArea.Etage.ETAGE_2, area.getFirstConstructibleEtage(ConstructionArea.Rang.RANG_3, limit2Etage));
+
+        // Rang 3 etage 2
+        area.addGradin(ConstructionArea.Rang.RANG_3, ConstructionArea.Etage.ETAGE_2);
+        Assertions.assertNull(area.getFirstConstructibleRang(limit2Etage));
+        Assertions.assertNull(area.getFirstConstructibleEtage(null, limit2Etage));
     }
 }
