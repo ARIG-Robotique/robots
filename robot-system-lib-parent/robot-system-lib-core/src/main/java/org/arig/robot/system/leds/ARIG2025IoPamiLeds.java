e97b8fec8d5102fc6e5a226737ba4fe49d7571f7
Gregory DEPUILLE <gregory.depuille@gmail.com>
2025-05-19 11:39:58 +0200

    Adds generic LED control
    Introduces a generic method for controlling individual LEDs using an ID, enhancing flexibility and maintainability.
    
    Replaces hardcoded LED IDs with the new enum.
    
    Simplifies the setting of LED colors by introducing a single entry point.

--- robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/leds/ARIG2025IoPamiLeds.java
+++ robot-system-lib-parent/robot-system-lib-core/src/main/java/org/arig/robot/system/leds/ARIG2025IoPamiLeds.java
@@ -27,9 +27,23 @@ public class ARIG2025IoPamiLeds {
         Yellow('Y'),
         Black('K');
 
-        @Getter
         @Accessors(fluent = true)
         private final char colorCode;
+    }
+
+    @RequiredArgsConstructor
+    public enum LedId {
+        ALL_LEDS((byte) 0),
+        LED1((byte) 1),
+        LED2((byte) 2),
+        LED3((byte) 3),
+        LED4((byte) 4),
+        LED5((byte) 5),
+        LED6((byte) 6),
+        LED7((byte) 7);
+
+        @Accessors(fluent = true)
+        private final byte id;
     }
 
     @Getter
@@ -47,13 +61,24 @@ public class ARIG2025IoPamiLeds {
         this.i2cManager = i2cManager;
     }
 
+    public void setLedColor(LedId led, LedColor color) {
+        // Reset led color
+        ledAll = ledAU = ledTeam = ledCalage = ledCentrale = null;
+
+        try {
+            i2cManager.sendData(deviceName, LED_REGISTER, led.id, (byte) color.colorCode);
+        } catch (I2CException e) {
+            log.error("Erreur lors de l'affichage des LEDs AU");
+        }
+    }
+
     public void setLedAU(LedColor color) {
         try {
             if (ledAU != color) {
                 log.info("Led AU : {} -> {}", ledAU != null ? ledAU.name() : "UNKNOWN", color.name());
                 ledAU = color;
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 1, (byte) color.colorCode);
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 7, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.LED1.id, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.LED7.id, (byte) color.colorCode);
             }
         } catch (I2CException e) {
             log.error("Erreur lors de l'affichage des LEDs AU");
@@ -65,8 +90,8 @@ public class ARIG2025IoPamiLeds {
             if (ledTeam != color) {
                 log.info("Led Team : {} -> {}", ledTeam != null ? ledTeam.name() : "UNKNOWN", color.name());
                 ledTeam = color;
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 2, (byte) color.colorCode);
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 6, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.LED2.id, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.LED6.id, (byte) color.colorCode);
             }
         } catch (I2CException e) {
             log.error("Erreur lors de l'affichage des LEDs Team");
@@ -78,8 +103,8 @@ public class ARIG2025IoPamiLeds {
             if (ledCalage != color) {
                 log.info("Led Calage : {} -> {}", ledCalage != null ? ledCalage.name() : "UNKNOWN", color.name());
                 ledCalage = color;
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 3, (byte) color.colorCode);
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 5, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.LED3.id, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.LED5.id, (byte) color.colorCode);
             }
         } catch (I2CException e) {
             log.error("Erreur lors de l'affichage des LEDs Calage");
@@ -91,7 +116,7 @@ public class ARIG2025IoPamiLeds {
             if (ledCentrale != color) {
                 log.info("Led centrale : {} -> {}", ledCentrale != null ? ledCentrale.name() : "UNKNOWN", color.name());
                 ledCentrale = color;
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 4, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.LED4.id, (byte) color.colorCode);
             }
         } catch (I2CException e) {
             log.error("Erreur lors de l'affichage de la LED centrale");
@@ -103,7 +128,7 @@ public class ARIG2025IoPamiLeds {
             if (ledAll != color) {
                 log.info("All Leds : {} -> {}", ledAll != null ? ledAll.name() : "UNKNOWN", color.name());
                 ledAll = ledAU = ledTeam = ledCalage = ledCentrale = color;
-                i2cManager.sendData(deviceName, LED_REGISTER, (byte) 0, (byte) color.colorCode);
+                i2cManager.sendData(deviceName, LED_REGISTER, LedId.ALL_LEDS.id, (byte) color.colorCode);
             }
         } catch (I2CException e) {
             log.error("Erreur lors de l'affichage de toutes les LEDs");
