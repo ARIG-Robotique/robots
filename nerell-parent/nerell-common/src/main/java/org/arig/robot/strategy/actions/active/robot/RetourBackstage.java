d5983fd2c77885edb0183f5af4722fd7add64ca3
Gregory DEPUILLE <gregory.depuille@gmail.com>
2025-05-28 11:21:14 +0200

    Adjusts robot backstage arrival sequence
    Tweaks robot's movements and servo actions upon reaching the backstage area.
    Specifically, ensures proper positioning and banner deployment by adjusting orientation, ascenseur, and pinces activation.

--- nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/robot/RetourBackstage.java
+++ nerell-parent/nerell-common/src/main/java/org/arig/robot/strategy/actions/active/robot/RetourBackstage.java
@@ -61,20 +61,22 @@ public class RetourBackstage extends AbstractNerellAction {
       mv.pathTo(entryPoint());
       if (position.getAngle() > 0) {
         // Face Avant
+        servosNerell.ascenseurAvantStock(false);
         if (rs.team() == Team.JAUNE) {
           mv.gotoOrientationDeg(180 - 35);
         } else {
           mv.gotoOrientationDeg(35);
         }
-        servosNerell.groupePincesAvantOuvert(false);
+        servosNerell.groupePincesAvantPrise(false);
       } else {
         // Face Arrière
+        servosNerell.ascenseurArriereStock(false);
         if (rs.team() == Team.JAUNE) {
           mv.gotoOrientationDeg(-35);
         } else {
           mv.gotoOrientationDeg(-180 + 35);
         }
-        servosNerell.groupePincesArriereOuvert(false);
+        servosNerell.groupePincesArrierePrise(false);
       }
       log.info("Arrivée au backstage");
       groups.forEach(g -> g.backstage(BackstageState.TARGET_REACHED));
