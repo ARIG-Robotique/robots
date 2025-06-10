5ac7d9a958ea49ff520a84ba6b785df586996ec4
Gregory DEPUILLE <gregory.depuille@gmail.com>
2025-06-10 19:28:28 +0200

    Upgrade to Gradle 8.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ description = "ARIG Association - Robots"
 
 tasks.wrapper {
     description = "Generates gradlew[.bat] scripts"
-    gradleVersion = "8.1.1"
+    gradleVersion = "8.14"
     distributionType = Wrapper.DistributionType.ALL
 }
 
